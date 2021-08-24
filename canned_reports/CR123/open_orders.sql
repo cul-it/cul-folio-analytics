9045bedb98ed91a408c7b995c39d9c01849cc6ee
NancyBolduc <57685173+NancyBolduc@users.noreply.github.com>
2021-08-24 16:09:19 -0400

    Update open_orders.sql

--- canned_reports/CR123/open_orders.sql
+++ canned_reports/CR123/open_orders.sql
@@ -49,27 +49,42 @@ WITH parameters AS (
         ''::varchar AS order_format, -- select 'Electronic Resource', 'Physical Resource', 'P/E Mix', 'Other' or leave blank for all
         ''::varchar AS instance_format_name, -- example: select e-resources vs physical (eg. "computer-online resource" for electronic resources or "Phycical Resource" for phycial resources)
         ''::varchar AS instance_mode_of_issuance, -- example: 'single unit', 'serial' etc. Mostly used by DBS
-        ''::varchar AS transaction_type -- example:'Payment', 'Pending payment'
+        ''::varchar AS transaction_type, -- example:'Payment', 'Pending payment' etc.
+        '':: VARCHAR AS fund_group_name  --Ex: Humanities, Social Sciences, Central etc. This is case sensitive
 ),
- funds_distr AS (
-    SELECT
-        id AS po_line_id,
-        json_extract_path_text(dist.data, 'code') AS fund_distribution_code,
-        json_extract_path_text(dist.DATA,'encumbrance AS fund_encumbrance'),
-        json_extract_path_text(dist.data, 'fundId') AS fund_distribution_id,
-        json_extract_path_text(dist.data, 'expenseClassId') AS fund_expense_class_id,
-        json_extract_path_text(dist.data, 'distributionType') AS fund_distribution_type,
-        json_extract_path_text(dist.data, 'value')::numeric AS fund_distribution_value
-    FROM
-        po_lines AS pol
-        CROSS JOIN json_array_elements(json_extract_path(data, 'fundDistribution')) WITH ORDINALITY AS dist(data)
+ fund_group_extract AS (
+	WITH parameters AS (
+	SELECT 
+	'':: VARCHAR AS fiscal_year_code --EX: FY2022, FY2023 etc.
+	),
+	transac_fund AS (
+	SELECT
+		transaction_id, 
+		transaction_amount,
+		CASE WHEN transaction_from_fund_code IS NULL THEN transaction_to_fund_code ELSE transaction_from_fund_code END AS transaction_fund_code,
+		CASE WHEN transaction_from_fund_id IS NULL THEN transaction_to_fund_id ELSE transaction_from_fund_id END AS transaction_fund_id
+	FROM folio_reporting.finance_transaction_invoices AS fti 
+	)
+		SELECT 
+			transaction_id,
+			transaction_amount,
+			fg.name AS fund_group_name,
+			transaction_fund_code,
+			ffy.code AS fiscal_year_code,
+			transaction_fund_id
+		FROM transac_fund AS trfund
+			LEFT JOIN finance_group_fund_fiscal_years AS fgffy ON fgffy.fund_id = trfund.transaction_fund_id
+			LEFT JOIN finance_fiscal_years AS ffy ON ffy.id = fgffy.fiscal_year_id 
+			LEFT JOIN finance_groups AS fg ON fg.id = fgffy.group_id 
+			WHERE ((ffy.code = (SELECT fiscal_year_code  FROM parameters)) OR ((SELECT fiscal_year_code  FROM parameters) = ''))
 )
     ---MAIN QUERY
 SELECT
    CURRENT_DATE,
-   ilfd.finance_fund_code,
-   ppo.po_number AS po_number,
+   fgextract.fund_group_name,
+   fgextract.transaction_fund_code,
    poi.created_date::date AS po_created_date,
+   ppo.po_number AS po_number,
    pol.po_line_number,
    poorg.org_name AS vendor_name,
    pol.title_or_package AS pol_title_or_package,
@@ -87,7 +102,8 @@ SELECT
    inv.payment_date,
    inv.vendor_invoice_no,
    frftp.transaction_encumbrance_initial_amount::NUMERIC(12,2),
-   fti.transaction_amount,
+   CASE WHEN fti.transaction_type = 'Credit' AND fti.transaction_amount >1 THEN fti.transaction_amount *-1 ELSE fti.transaction_amount END AS transaction_amount2,
+   --fti.transaction_id,
    fti.transaction_currency,
    fti.transaction_type
 FROM
@@ -104,41 +120,49 @@ FROM
 	LEFT JOIN folio_reporting.po_instance AS poi ON poi.po_line_number = pol.po_line_number
 	LEFT JOIN folio_reporting.finance_transaction_invoices AS fti ON fti.invoice_line_id=invl.id AND ilfd.finance_fund_code=fti.transaction_from_fund_code
 	LEFT JOIN folio_reporting.finance_transaction_purchase_order AS frftp ON frftp.pol_number = pol.po_line_number
+	LEFT JOIN fund_group_extract AS fgextract ON fgextract.transaction_fund_code = ilfd.finance_fund_code
 WHERE  
 	ppo.workflow_status LIKE 'Open'
-    AND (ppo.order_type = (SELECT order_type FROM parameters) OR (SELECT order_type FROM parameters) = '')
-    AND (pol.order_format = (SELECT order_format FROM parameters) OR (SELECT order_format FROM parameters) = '')
-    AND (ifo.format_name = (SELECT instance_format_name FROM parameters) OR (SELECT instance_format_name FROM parameters) = '')
-    AND (iext.mode_of_issuance_name = (SELECT instance_mode_of_issuance FROM parameters) OR (SELECT instance_mode_of_issuance FROM parameters) = '')
-    AND (fti.transaction_type = (SELECT transaction_type FROM parameters) OR (SELECT transaction_type FROM parameters) = '')
+    	AND (ppo.order_type = (SELECT order_type FROM parameters) OR (SELECT order_type FROM parameters) = '')
+    	AND (pol.order_format = (SELECT order_format FROM parameters) OR (SELECT order_format FROM parameters) = '')
+    	AND (ifo.format_name = (SELECT instance_format_name FROM parameters) OR (SELECT instance_format_name FROM parameters) = '')
+    	AND (iext.mode_of_issuance_name = (SELECT instance_mode_of_issuance FROM parameters) OR (SELECT instance_mode_of_issuance FROM parameters) = '')
+    	AND (fti.transaction_type = (SELECT transaction_type FROM parameters) OR (SELECT transaction_type FROM parameters) = '')
+	AND (fgextract.fund_group_name = (SELECT fund_group_name FROM parameters) OR (SELECT fund_group_name FROM parameters) = '')
 GROUP BY
 	ppo.order_type,
    	ilfd.finance_fund_code,
    	pol.po_line_number,
 	pol.title_or_package,
 	poi.title,
-    pol.order_format,
-    pol.receipt_status,
-    ifo.format_name,
-    invl.id,
-    invl.invoice_line_status,
-    invl.total,
-    ppo.po_number,
-    ppo.workflow_status,
-    poorg.org_name,
-    pol.payment_status,
-    pol_phys_type.pol_mat_type_name,
-    pol_er_type.pol_er_mat_type_name,
-    poi.pol_instance_hrid,
-   	ilfd.invoice_line_total,
-    inv.currency,
-    inv.vendor_invoice_no,
+    	pol.order_format,
+   	pol.receipt_status,
+    	ifo.format_name,
+    	invl.id,
+    	invl.invoice_line_status,
+    	invl.total,
+    	ppo.po_number,
+    	ppo.workflow_status,
+    	poorg.org_name,
+    	pol.payment_status,
+    	pol_phys_type.pol_mat_type_name,
+    	pol_er_type.pol_er_mat_type_name,
+    	poi.pol_instance_hrid,
+    	ilfd.invoice_line_total,
+    	inv.currency,
+    	inv.vendor_invoice_no,
 	fti.transaction_amount,
 	fti.transaction_currency,
 	fti.transaction_type,
 	frftp.transaction_encumbrance_initial_amount,
 	inv.payment_date,
-	poi.created_date
+	poi.created_date,
+	fgextract.fund_group_name,
+	fgextract.transaction_fund_code,
+	fti.transaction_id
 ORDER BY 
-	finance_fund_code ASC;
+	fund_group_name,
+	finance_fund_code ASC,
+	vendor_name,
+	po_number;
 	
