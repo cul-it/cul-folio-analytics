8eebf11406e338dd1709f4cc6e77303c63a3bd40
Vandana Shah <vp25@cornell.edu>
2021-12-10 15:46:55 -0500

    Updated for CUL needs.

--- canned_reports/CR127/Borrow_direct_interlibrary_loan_overdue_items.sql
+++ canned_reports/CR127/Borrow_direct_interlibrary_loan_overdue_items.sql
@@ -1,55 +1,107 @@
 WITH parameters AS (
-    SELECT
-        /* replace the placeholder number with the number of days overdue that is needed for this report */
-        '20'::integer AS days_overdue_filter, -- doesn't work if empty
-        /* Fill in a location name, or leave blank for all locations */
-          ''::varchar AS items_permanent_location_filter, --Olin, ILR, Africana, etc.
-        /*Fill in 1-4 borrower patron group names*/
-         'Borrow Direct'::varchar AS borrower_patron_group_name_filter1, -- Borrow Direct
-         'Interlibrary Loan'::varchar AS borrower_patron_group_name_filter2, -- Interlibrary Loan
-         ''::varchar AS borrower_patron_group_name_filter3, -- Faculty
-         ''::varchar AS borrower_patron_group_name_filter4 -- Graduate
-        ),
-days AS (
-    SELECT 
-        loan_id,
-        DATE_PART('day', NOW() - loan_due_date) AS days_overdue
-    FROM folio_reporting.loans_items
-)
 SELECT
-    li.patron_group_name as borrower_patron_group_name,
- -- ug.user_id AS borrower_id,
-    ug.barcode AS borrower_barcode,
- -- li.loan_due_date,
-    days.days_overdue, 
- -- he.call_number,
-    ie.barcode AS item_barcode,
-    ins.title,
-  --ie.material_type_name,
-    ie.permanent_location_name
-  --ie.effective_location_name
-   FROM
-	folio_reporting.loans_items as li
- LEFT JOIN folio_reporting.item_ext AS ie
-	ON li.item_id = ie.item_id
-LEFT JOIN folio_reporting.users_groups AS ug 
-	ON ug.patron_group=li.patron_group_id_at_checkout
-LEFT JOIN folio_reporting.holdings_ext AS he
-	ON ie.holdings_record_id=he.holdings_id
-LEFT JOIN public.user_users AS uu
-	ON li.user_id=uu.id
-LEFT JOIN days ON days.loan_id=li.loan_id
-LEFT JOIN public.inventory_instances AS ins 
-	ON he.instance_id=ins.id
-    WHERE (days.days_overdue > 0 AND days.days_overdue <= (SELECT days_overdue_filter FROM parameters))	
-     AND 
-        li.patron_group_name IN ((SELECT borrower_patron_group_name_filter1 FROM parameters),
-                      			(SELECT borrower_patron_group_name_filter2 FROM parameters),
-                      			(SELECT borrower_patron_group_name_filter3 FROM parameters),
-                      			(SELECT borrower_patron_group_name_filter4 FROM parameters)
-                    )
-    AND (
-    ie.permanent_location_name = (SELECT items_permanent_location_filter FROM parameters)
-        OR '' = (SELECT items_permanent_location_filter FROM parameters)
-    )
-    ;
+	/* replace the placeholder number with the number of days overdue that is needed for this report */
+	'30'::integer AS days_overdue_filter
+	-- doesn't work if empty
+       ),
+days AS (
+SELECT
+	loan_id,
+	item_id,
+	DATE_PART('day', NOW() - loan_due_date) AS days_overdue
+FROM
+	folio_reporting.loans_items
+),
+BDILL AS 
+(
+SELECT
+	uu.id,
+	json_extract_path_text(uu.data,
+	'personal',
+	'lastName') AS patron_last_name,
+	json_extract_path_text(uu.data,
+	'personal',
+	'firstName') AS patron_first_name,
+	ug.desc AS patron_group_name,
+	uu.barcode
+FROM
+	user_users AS uu
+LEFT JOIN user_groups AS ug 
+        ON
+	uu.patron_group = ug.id
+WHERE
+	ug.desc LIKE 'Borrow Direct%'
+	OR ug.desc LIKE 'Inter%'
+)
+
+SELECT
+	to_char(current_date::DATE, 'mm/dd/yyyy') AS todays_date,
+	days.days_overdue,
+	--  DATE_PART('day', NOW() - li.loan_due_date) AS days_overdue,
+	li.loan_policy_name,
+	BDILL.patron_last_name,
+	BDILL.patron_first_name,
+	BDILL.barcode AS patron_barcode,
+	ie.title,
+	he.permanent_location_name,
+	he.call_number,
+	li.enumeration,
+	li.chronology,
+	li.copy_number,
+	li.barcode AS item_barcode,
+	max(li.loan_date)AS latest_loan_date,
+	to_char(li.loan_due_date::DATE, 'mm/dd/yyyy') AS loan_due_date,
+	ite.status_name,
+	to_char(ite.status_date::DATE, 'mm/dd/yyyy') AS item_status_date
+FROM
+	folio_reporting.loans_items AS li
+LEFT JOIN BDILL 
+        ON
+	li.user_id = BDILL.id
+LEFT JOIN days ON
+	days.loan_id = li.loan_id
+LEFT JOIN folio_reporting.holdings_ext AS he 
+        ON
+	li.holdings_record_id = he.holdings_id
+LEFT JOIN folio_reporting.instance_ext AS ie 
+        ON
+	he.instance_id = ie.instance_id
+LEFT JOIN folio_reporting.item_ext AS ite 
+        ON
+	li.item_id = ite.item_id
+WHERE
+	(days.days_overdue > 0
+		AND days.days_overdue <= (
+		SELECT
+			days_overdue_filter
+		FROM
+			parameters))
+	AND li.loan_policy_name IN ('20 weeks (ILL)', '20 weeks (BD)')
+	AND li.loan_return_date IS NULL
+GROUP BY
+	to_char(current_date::DATE, 'mm/dd/yyyy'),
+	li.loan_policy_name,
+	li.loan_date,
+	BDILL.patron_last_name,
+	BDILL.patron_first_name,
+	BDILL.barcode,
+	ie.title,
+	he.permanent_location_name,
+	he.call_number,
+	li.enumeration,
+	li.chronology,
+	li.copy_number,
+	li.barcode,
+	days.days_overdue,
+	to_char(li.loan_date::DATE, 'mm/dd/yyyy'),
+	to_char(li.loan_due_date::DATE, 'mm/dd/yyyy'),
+	ite.status_name,
+	to_char(ite.status_date::DATE, 'mm/dd/yyyy')
+ORDER BY
+	patron_last_name,
+	patron_first_name,
+	loan_date,
+	title,
+	enumeration,
+	chronology;
+
