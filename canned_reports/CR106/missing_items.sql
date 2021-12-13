5d389b0268f29cee2f73200f74ce30ef8c718eff
Vandana Shah <vp25@cornell.edu>
2021-12-13 13:35:21 -0500

    Update missing_items.sql
    Updated to include sizes

--- canned_reports/CR106/missing_items.sql
+++ canned_reports/CR106/missing_items.sql
@@ -1,11 +1,11 @@
 WITH parameters AS (
     SELECT 
         '01-01-2021'::date AS start_date,
-        '11-18-2021'::date AS end_date,
+        '12-11-2021'::date AS end_date,
         /*Enter item status*/
         'Missing'::varchar AS item_status_filter, --Missing 
               ---- Fill out a location or an owning library filter ----
-         ''::varchar AS owning_library_name_filter -- Examples: Olin Library, Library Annex, etc.
+         'Olin Library'::varchar AS owning_library_name_filter -- Examples: Olin Library, Library Annex, etc.
         ),
 ---------- SUB-QUERIES/TABLES ----------
 item_subset AS (
@@ -16,7 +16,7 @@ item_subset AS (
         status_name AS item_status,
         status_date::timestamp AS item_status_date,
         barcode,
-        call_number,
+        --call_number,
         enumeration,
         chronology,
         copy_number,
@@ -32,7 +32,7 @@ item_subset AS (
 item_notes_list AS (
     SELECT
         itn.item_id,
-        string_agg(DISTINCT itn.note, '|') AS notes_list
+        string_agg(DISTINCT itn.note, ' | ') AS notes_list
     FROM
         folio_reporting.item_notes AS itn
         RIGHT JOIN item_subset AS its ON itn.item_id = its.item_id
@@ -47,7 +47,27 @@ instance_subset AS (
         LEFT JOIN folio_reporting.holdings_ext AS he ON its.holdings_record_id = he.holdings_id
         LEFT JOIN folio_reporting.instance_ext AS ie ON he.instance_id = ie.instance_id
 ),
-publication_dates_list AS (
+
+"size" as (
+select 
+        srs.instance_id as srs_instance_id,
+        srs.instance_hrid,
+        string_agg(srs."content", ' | ') as pagination_size,
+        srs.field 
+
+from srs_marctab as srs 
+        left join instance_subset on
+        instance_subset.instance_id = srs.instance_id 
+
+where srs.field = '300'
+
+group by 
+        srs.instance_id,
+        srs.instance_hrid,
+        srs.field
+)
+
+/*publication_dates_list AS (
     SELECT
         ip.instance_id,
         string_agg(DISTINCT date_of_publication, '|') AS publication_dates_list
@@ -56,26 +76,31 @@ publication_dates_list AS (
         RIGHT JOIN instance_subset AS ins ON ip.instance_id = ins.instance_id
     GROUP BY
         ip.instance_id
-)
+)*/
     ---------- MAIN QUERY ----------
 SELECT DISTINCT
     (SELECT start_date::varchar FROM parameters) || 
         ' to ' || 
         (SELECT end_date::varchar FROM parameters) AS date_range,
     ll.library_name as owning_library_name,
-  --  ite.permanent_location_name AS item_permanent_location_name,
     he.permanent_location_name as holdings_perm_loc_name,
-    ite.effective_call_number,
+    he.call_number_prefix,
+    he.call_number,
+    he.call_number_suffix,
+    --ite.effective_call_number,
     its.enumeration,
     its.chronology,
     its.copy_number,
-    its.volume,
+    --its.volume,
     its.barcode,
+    size.pagination_size,
     ie.title,
+  --  ite.permanent_location_name AS item_permanent_location_name,
+    
     its.item_status,
     TO_CHAR(its.item_status_date :: date,'mm/dd/yyyy')AS item_status_date,
     ite.material_type_name,
-    pd.publication_dates_list,
+    --pd.publication_dates_list,
     nl.notes_list,
     ii.effective_shelving_order
   FROM
@@ -87,9 +112,11 @@ SELECT DISTINCT
     LEFT JOIN folio_reporting.holdings_ext AS he ON its.holdings_record_id = he.holdings_id
     left join folio_reporting.locations_libraries as ll on he.permanent_location_id = ll.location_id
     LEFT JOIN folio_reporting.instance_ext AS ie ON he.instance_id = ie.instance_id
-    LEFT JOIN publication_dates_list AS pd ON ie.instance_id = pd.instance_id
+    left join "size" on ie.instance_id = "size".srs_instance_id
+   
+    --LEFT JOIN publication_dates_list AS pd ON ie.instance_id = pd.instance_id
 WHERE 
      (ll.library_name = 
         (SELECT owning_library_name_filter FROM parameters)
         OR (SELECT owning_library_name_filter FROM parameters) = '')
-  ORDER BY owning_library_name, holdings_perm_loc_name, effective_shelving_order ;
+  ORDER BY owning_library_name, holdings_perm_loc_name, material_type_name, effective_shelving_order ;
