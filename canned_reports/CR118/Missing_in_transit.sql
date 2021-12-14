f28f7ccc9567613656728a35892fddc981637340
Vandana Shah <vp25@cornell.edu>
2021-12-14 14:39:18 -0500

    Update Missing_in_transit.sql

--- canned_reports/CR118/Missing_in_transit.sql
+++ canned_reports/CR118/Missing_in_transit.sql
@@ -1,56 +1,109 @@
 WITH parameters AS (
     SELECT
-  /*Days in transit filter is the number of days the item has been 'in transit', starting with today*/  
-    
-   		'7'::integer AS days_in_transit_filter, -- doesn't work if empty
-        'In transit'::VARCHAR AS item_status_filter, --  'Checked out', 'Available', 'In transit'
-        ---- Fill out one location or service point filter, leave others blank ----
-        'Library Annex'::varchar AS owning_library_filter -- 'Olin, Mann, etc.'
-),
-days AS (
-    SELECT 
-        item_id,
-        DATE_PART('day', NOW() - date(status_date)) AS days_in_transit
-    FROM folio_reporting.item_ext
-),
-item_notes_list AS (
-    SELECT
-        item_id,
-        string_agg(DISTINCT note, '|'::text) AS notes_list
+        		
+        ---- Fill out owning library filter ----
+        'Library Annex'::varchar AS owning_library_filter -- 'Olin Library, Mann Library, etc.'
         
-    FROM
-        folio_reporting.item_notes
-    GROUP BY
-        item_id
-)
-    ---------- MAIN QUERY ----------
-    SELECT
-        TO_CHAR(ie.status_date :: date,'mm/dd/yyyy')AS status_date,
-        days.days_in_transit,
-        ine.title,
-        ie.status_name,
-        ie.in_transit_destination_service_point_name,
-        ie.barcode,
+),
+
+audit1 AS 
+                (SELECT
+                    items.ordinality,
+                    json_extract_path_text(items.data, 'itemBarcode') AS item_barcode,
+                    max(acl.date) as latest_action_date
+                FROM
+                    audit_circulation_logs as acl
+                    CROSS JOIN LATERAL json_array_elements(json_extract_path(data, 'items')) WITH ORDINALITY AS items (data)
+                
+                WHERE acl.description like '%n transit%'
+                
+                GROUP BY 
+                    items.ordinality,
+                    json_extract_path_text(items.data, 'itemBarcode')            
+                    ),
+audit2 AS 
+                (SELECT
+                    acl.service_point_id,
+                    items.ordinality,
+                    json_extract_path_text(items.data, 'itemBarcode') AS audit2_item_barcode,
+                    acl.date AS audit2_action_date
+                FROM
+                    audit_circulation_logs AS acl
+                    CROSS JOIN LATERAL json_array_elements(json_extract_path(data, 'items')) WITH ORDINALITY AS items (data)
+                
+                WHERE acl.description LIKE '%n transit%'
+),
+                
+servpt AS 
+                (SELECT distinct
+                        audit1.item_barcode,
+                        audit1.latest_action_date,
+                        max(audit2.audit2_action_date),
+                        audit2.audit2_item_barcode,
+                        audit2.service_point_id,
+                        isp.id,
+                        isp.name AS discharge_service_point
+                        
+                FROM
+                        audit1
+                        INNER JOIN audit2 ON audit2.audit2_action_date = audit1.latest_action_date
+                        INNER JOIN inventory_service_points AS isp ON audit2.service_point_id = isp.id
+                GROUP BY 
+                        audit1.item_barcode,
+                        audit1.latest_action_date,
+                        audit2.audit2_item_barcode,
+                        audit2.service_point_id,
+                        isp.id,
+                        isp.name)
+    
+---------MAIN QUERY---------
+
+SELECT distinct
+        ll.library_name,
+        ixt.instance_hrid,
+        he.holdings_hrid,
+        iext.item_hrid,
+        ixt.title,
+        iext.barcode,
+        he.permanent_location_name,
+        iext.effective_location_name as item_effective_location,
+        he.call_number_prefix,
         he.call_number,
-        ie.enumeration,
-        ie.chronology,
-        ie.copy_number,
-        ie.volume,
-        ll.library_name  AS owning_library_name,
-        he.permanent_location_name AS holdings_permanent_location_name,
-        nl.notes_list,
-        ie.material_type_name
-        FROM
-        folio_reporting.item_ext AS ie
-        LEFT JOIN days ON ie.item_id=days.item_id
-        LEFT JOIN item_notes_list AS nl ON ie.item_id = nl.item_id
-        LEFT JOIN folio_reporting.holdings_ext AS he ON ie.holdings_record_id = he.holdings_id
-        LEFT JOIN folio_reporting.instance_ext AS ine ON he.instance_id = ine.instance_id
-        LEFT JOIN folio_reporting.locations_libraries AS ll ON he.permanent_location_id=ll.location_id
-        WHERE (days.days_in_transit > 0 AND days.days_in_transit >= (SELECT days_in_transit_filter FROM parameters))
-        	AND (ll.library_name = (SELECT owning_library_filter FROM parameters)
-            	OR (SELECT owning_library_filter FROM parameters) = '')
-            AND ie.status_name = 'In transit'
-                                      
-            ORDER BY call_number, enumeration, chronology, copy_number
-                      ;
+        he.call_number_suffix,
+        iext.enumeration,
+        iext.chronology,
+        iext.copy_number,
+        iext.material_type_name,
+        iext.status_name,
+        to_char(CURRENT_DATE::DATE,'mm/dd/yyyy') AS todays_date,
+        to_char(iext.status_date::DATE,'mm/dd/yyyy') AS item_status_date,
+        DATE_PART('day', NOW() - date(iext.status_date::DATE)) AS days_in_transit,
+        servpt.discharge_service_point,
+        iext.in_transit_destination_service_point_name,
+        ii.effective_shelving_order
+
+FROM folio_reporting.instance_ext as ixt 
+        LEFT JOIN folio_reporting.holdings_ext as he 
+        ON ixt.instance_id = he.instance_id 
+        
+        LEFT JOIN folio_reporting.item_ext as iext 
+        ON he.holdings_id = iext.holdings_record_id
+        
+        LEFT JOIN servpt
+        ON iext.barcode = servpt.item_barcode
+        
+        left join audit1 
+        on iext.status_date::DATE = audit1.latest_action_date::DATE
+        
+        LEFT JOIN inventory_items as ii 
+        ON iext.item_id = ii.id
+        
+        LEFT JOIN folio_reporting.locations_libraries as ll 
+        ON he.permanent_location_id = ll.location_id 
+ 
+        WHERE ll.library_name = (SELECT owning_library_filter FROM parameters)
+        AND  iext.status_name = 'In transit'
+            	
+ORDER BY permanent_location_name, effective_shelving_order, barcode
+;
+
