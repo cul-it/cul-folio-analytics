720bcb49d7251dbfa05b781ed4d9f0b3da327b69
Vandana Shah <vp25@cornell.edu>
2022-01-14 11:34:40 -0500

    Update loans_counts.sql
    Updated to include item effective location as the main location filter.

--- canned_reports/CR100/loans_counts.sql
+++ canned_reports/CR100/loans_counts.sql
@@ -11,31 +11,38 @@ WITH parameters AS (
         '2021-07-01'::date AS start_date,
         '2022-06-30'::date AS end_date,
         /* Fill one out, leave others blank to filter by location */
-        ''::varchar AS items_permanent_location_filter, -- Examples: Olin, ILR, Africana, etc.
+        ''::varchar AS item_location_filter, -- Examples: Olin, ILR, Africana, etc.
         ''::varchar AS library_filter -- Examples: Nestle Library', Library Annex, etc.
 )
     --MAIN QUERY
+    
 SELECT (
     SELECT start_date::varchar FROM parameters) || ' to '::varchar || (SELECT end_date::varchar FROM parameters) AS date_range,
-    li.current_item_permanent_location_library_name AS owning_library_name, 
-    li.current_item_permanent_location_name AS permanent_location_name,
+    ll.library_name,
+    li.current_item_effective_location_name AS location_name,
     li.patron_group_name AS patron_group_name,
     li.material_type_name AS material_type_name,
     li.loan_policy_name AS loan_policy_name,
-    count (li.loan_id) as number_of_loans
-   FROM
+    count (li.loan_id) AS number_of_loans
+   
+FROM
     folio_reporting.loans_items AS li
+    left join folio_reporting.locations_libraries as ll 
+    on li.current_item_effective_location_id = ll.location_id
+
 WHERE
     loan_date >= (SELECT start_date FROM parameters)
     AND loan_date < (SELECT end_date FROM parameters)
-    AND (li.current_item_permanent_location_name = (SELECT items_permanent_location_filter FROM parameters)
-         OR '' = (SELECT items_permanent_location_filter FROM parameters))
-     AND (li.current_item_permanent_location_library_name = (SELECT library_filter FROM parameters)
+    AND (li.current_item_effective_location_name = (SELECT item_location_filter FROM parameters)
+         OR '' = (SELECT item_location_filter FROM parameters))
+     AND (ll.library_name = (SELECT library_filter FROM parameters)
          OR '' = (SELECT library_filter FROM parameters))
  GROUP BY 
-	li.patron_group_name,
+    ll.library_name,
+    li.current_item_effective_location_name,
+    li.patron_group_name,
     li.material_type_name,
-    li.loan_policy_name,
-    li.current_item_permanent_location_name,
-    li.current_item_permanent_location_library_name 
-ORDER BY patron_group_name, loan_policy_name, material_type_name;
+    li.loan_policy_name
+
+ ORDER BY library_name, location_name, patron_group_name, loan_policy_name, material_type_name;
+
