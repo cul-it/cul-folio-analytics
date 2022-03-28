36d48f52cd3ef0a1458af47a72d80ac0b28bffe5
Natalya Pikulik <62571646+natalyapik@users.noreply.github.com>
2022-03-28 11:21:09 -0400

    Update hathi_serials.sql

--- canned_reports/CR143_A/hathi_serials.sql
+++ canned_reports/CR143_A/hathi_serials.sql
@@ -1,6 +1,6 @@
 --1----selects pull of records based on ldr and filters certain locations-----------
-DROP TABLE IF EXISTS LOCAL.h_s_1; 
-CREATE TABLE LOCAL.h_s_1 AS
+DROP TABLE IF EXISTS local_hathi.h_s_1; 
+CREATE TABLE local_hathi.h_s_1 AS
 SELECT
 DISTINCT sm.instance_hrid,
     sm.instance_id,
@@ -14,24 +14,24 @@ DISTINCT sm.instance_hrid,
     FROM srs_marctab sm 
     LEFT JOIN folio_reporting.holdings_ext he ON sm.instance_id = he.instance_id
     LEFT JOIN srs_records sr ON sm.srs_id = sr.id
-    where sr.state  = 'ACTUAL'
-    WHERE (sm.field = '000' AND substring(sm.content, 7, 2) IN ('as'))
+    WHERE sr.state  = 'ACTUAL'
+    AND (sm.field = '000' AND substring(sm.content, 7, 2) IN ('as'))
     AND he.permanent_location_name !~~ 'serv,remo'
     AND he.permanent_location_name !~~ 'Borrow Direct'
     AND he.permanent_location_name !~~* '%LTS%'
 ;
-CREATE INDEX ON local.h_s_1 (instance_hrid);
-CREATE INDEX ON local.h_s_1 (instance_id);
-CREATE INDEX ON local.h_s_1 (field);
-CREATE INDEX ON local.h_s_1 (sf);
-CREATE INDEX ON local.h_s_1 (content);
-CREATE INDEX ON local.h_s_1 (permanent_location_name);
-CREATE INDEX ON local.h_s_1 (call_number);
-CREATE INDEX ON local.h_s_1 (discovery_suppress);
+CREATE INDEX ON local_hathi.h_s_1 (instance_hrid);
+CREATE INDEX ON local_hathi.h_s_1 (instance_id);
+CREATE INDEX ON local_hathi.h_s_1 (field);
+CREATE INDEX ON local_hathi.h_s_1 (sf);
+CREATE INDEX ON local_hathi.h_s_1 (content);
+CREATE INDEX ON local_hathi.h_s_1 (permanent_location_name);
+CREATE INDEX ON local_hathi.h_s_1 (call_number);
+CREATE INDEX ON local_hathi.h_s_1 (discovery_suppress);
 
 --2------------selects/deselects records with 945 (monoseries standing orders) and filters it from h_s_1 table-------------------------------------
-DROP TABLE IF EXISTS local.h_s_2;
-CREATE TABLE LOCAL.h_s_2 AS
+DROP TABLE IF EXISTS local_hathi.h_s_2;
+CREATE TABLE local_hathi.h_s_2 AS
 WITH ninefortyfive AS 
 (SELECT
     sm.instance_id
@@ -45,18 +45,18 @@ WITH ninefortyfive AS
     hsr.instance_id,
     hsr.permanent_location_name,
     hsr.call_number 
-    FROM LOCAL.h_s_1 hsr
+    FROM local_hathi.h_s_1 hsr
     LEFT JOIN ninefortyfive n ON hsr.instance_id = n.instance_id
     WHERE n.instance_id IS NULL
 ;
-CREATE INDEX ON local.h_s_2(instance_hrid);
-CREATE INDEX ON local.h_s_2(instance_id);
-CREATE INDEX ON local.h_s_2(permanent_location_name);
-CREATE INDEX ON local.h_s_2(call_number);
+CREATE INDEX ON local_hathi.h_s_2(instance_hrid);
+CREATE INDEX ON local_hathi.h_s_2(instance_id);
+CREATE INDEX ON local_hathi.h_s_2(permanent_location_name);
+CREATE INDEX ON local_hathi.h_s_2(call_number);
 
 --3------------------------selects/deselects records with 245 $h[electronic resource] and filters from h_s_2 tabe------------------------
-DROP TABLE IF EXISTS LOCAL.h_s_3;   
-CREATE TABLE LOCAL.h_s_3 AS 
+DROP TABLE IF EXISTS local_hathi.h_s_3;   
+CREATE TABLE local_hathi.h_s_3 AS 
 WITH twofortyfive AS (
 SELECT
     sm.instance_hrid,
@@ -80,18 +80,18 @@ SELECT
     h.instance_hrid,
     h.permanent_location_name,
     h.call_number
-    FROM LOCAL.h_s_2 h 
+    FROM local_hathi.h_s_2 h 
     LEFT JOIN twofortyfive t ON h.instance_id = t.instance_id
     WHERE t.instance_id IS NULL
 ;
-CREATE INDEX ON local.h_s_3 (instance_id);
-CREATE INDEX ON local.h_s_3 (instance_hrid);
-CREATE INDEX ON local.h_s_3 (permanent_location_name);
-CREATE INDEX ON local.h_s_3 (call_number);
+CREATE INDEX ON local_hathi.h_s_3 (instance_id);
+CREATE INDEX ON local_hathi.h_s_3 (instance_hrid);
+CREATE INDEX ON local_hathi.h_s_3 (permanent_location_name);
+CREATE INDEX ON local_hathi.h_s_3 (call_number);
 
 --4---------------------selects/deselects records with 336 $aunmediated content and filters from h_s_3----------------  
-DROP TABLE IF EXISTS LOCAL.h_s_4;
-CREATE TABLE LOCAL.h_s_4 AS
+DROP TABLE IF EXISTS local_hathi.h_s_4;
+CREATE TABLE local_hathi.h_s_4 AS
 WITH threethirtysix AS (
 SELECT 
     sm.instance_id,
@@ -108,18 +108,18 @@ SELECT
     h.instance_hrid,
     h.permanent_location_name,
     h.call_number 
-    FROM LOCAL.h_s_3 h
+    FROM local_hathi.h_s_3 h
     LEFT JOIN threethirtysix t ON h.instance_id = t.instance_id
     WHERE t.instance_id IS NULL
 ;
-CREATE INDEX ON local.h_s_4 (instance_id);
-CREATE INDEX ON local.h_s_4 (instance_hrid);
-CREATE INDEX ON local.h_s_4 (permanent_location_name);
-CREATE INDEX ON local.h_s_4 (call_number);
+CREATE INDEX ON local_hathi.h_s_4 (instance_id);
+CREATE INDEX ON local_hathi.h_s_4 (instance_hrid);
+CREATE INDEX ON local_hathi.h_s_4 (permanent_location_name);
+CREATE INDEX ON local_hathi.h_s_4 (call_number);
 
 --5--------------------selects/deselects records with 300 $amap or maps and filters from h_s_4------------------ 
-DROP TABLE IF EXISTS LOCAL.h_s_5;
-CREATE TABLE LOCAL.h_s_5 AS
+DROP TABLE IF EXISTS local_hathi.h_s_5;
+CREATE TABLE local_hathi.h_s_5 AS
 WITH threehundred AS 
 (SELECT
     sm.instance_hrid,
@@ -144,18 +144,18 @@ WITH threehundred AS
     h.permanent_location_name,
     h.call_number,
     t.discovery_suppress
-    FROM LOCAL.h_s_4 h
+    FROM local_hathi.h_s_4 h
     left JOIN threehundred t ON h.instance_id = t.instance_id
     WHERE t.instance_id IS NULL
 ;
-CREATE INDEX ON local.h_s_5 (instance_id);
-CREATE INDEX ON local.h_s_5 (instance_hrid);
-CREATE INDEX ON local.h_s_5 (permanent_location_name);
-CREATE INDEX ON local.h_s_5 (call_number);
-CREATE INDEX ON LOCAL.h_s_5 (discovery_suppress);
+CREATE INDEX ON local_hathi.h_s_5 (instance_id);
+CREATE INDEX ON local_hathi.h_s_5 (instance_hrid);
+CREATE INDEX ON local_hathi.h_s_5 (permanent_location_name);
+CREATE INDEX ON local_hathi.h_s_5 (call_number);
+CREATE INDEX ON local_hathi.h_s_5 (discovery_suppress);
 --6-----------------------filters suppressed holding records with the note "decision - no"-----------
-DROP TABLE IF EXISTS LOCAL.h_s_6;
-CREATE TABLE LOCAL.h_s_6 AS
+DROP TABLE IF EXISTS local_hathi.h_s_6;
+CREATE TABLE local_hathi.h_s_6 AS
 WITH holdings_note AS
 (SELECT 
     hn.note,
@@ -164,7 +164,7 @@ WITH holdings_note AS
     hn.holdings_hrid,
     hn.holdings_id,
     h.discovery_suppress 
-    FROM LOCAL.h_s_5 h 
+    FROM local_hathi.h_s_5 h 
     LEFT JOIN folio_reporting.holdings_notes hn ON h.instance_id= hn.instance_id 
     WHERE hn.note ilike ('%decision%%no%')
     AND h.discovery_suppress IS NULL 
@@ -176,25 +176,25 @@ SELECT
     h.call_number,
     hn.note,
     hn.discovery_suppress
-    FROM LOCAL.h_s_5 h
+    FROM local_hathi.h_s_5 h
     left JOIN holdings_note hn ON h.instance_id = hn.instance_id
     WHERE hn.instance_id IS NULL;
-CREATE INDEX ON local.h_s_6 (instance_id);
-CREATE INDEX ON local.h_s_6 (instance_hrid);
-CREATE INDEX ON local.h_s_6 (permanent_location_name);
-CREATE INDEX ON local.h_s_6 (call_number);
-CREATE INDEX ON local.h_s_6 (discovery_suppress);
+CREATE INDEX ON local_hathi.h_s_6 (instance_id);
+CREATE INDEX ON local_hathi.h_s_6 (instance_hrid);
+CREATE INDEX ON local_hathi.h_s_6 (permanent_location_name);
+CREATE INDEX ON local_hathi.h_s_6 (call_number);
+CREATE INDEX ON local_hathi.h_s_6 (discovery_suppress);
 ;
 
 --7-----------------------filters records by certain values in call number from h_s_6-------------------
-DROP TABLE IF EXISTS LOCAL.h_s_7;
-CREATE TABLE LOCAL.h_s_7 AS
+DROP TABLE IF EXISTS local_hathi.h_s_7;
+CREATE TABLE local_hathi.h_s_7 AS
 SELECT 
     hhn.instance_hrid,
     hhn.instance_id,
     hhn.call_number,
     hhn.permanent_location_name 
-    FROM LOCAL.h_s_6 hhn  
+    FROM local_hathi.h_s_6 hhn  
     WHERE hhn.call_number !~~* 'on order%'
     AND hhn.call_number !~~* 'in process%'
     AND hhn.call_number !~~* 'Available for the library to purchase'
@@ -202,14 +202,14 @@ SELECT
     AND hhn.call_number !~~* '%fiche%'
     AND hhn.call_number !~~* 'On selector%'
 ;
-CREATE INDEX ON local.h_s_7 (instance_hrid);
-CREATE INDEX ON local.h_s_7 (instance_id);
-CREATE INDEX ON local.h_s_7 (call_number);
-CREATE INDEX ON local.h_s_7 (permanent_location_name);
+CREATE INDEX ON local_hathi.h_s_7 (instance_hrid);
+CREATE INDEX ON local_hathi.h_s_7 (instance_id);
+CREATE INDEX ON local_hathi.h_s_7 (call_number);
+CREATE INDEX ON local_hathi.h_s_7 (permanent_location_name);
 
 --8--------------------filters records for presents of oclc number------------------    
-DROP TABLE IF EXISTS LOCAL.h_s_8;
-CREATE TABLE LOCAL.h_s_8 AS
+DROP TABLE IF EXISTS local_hathi.h_s_8;
+CREATE TABLE local_hathi.h_s_8 AS
 WITH oclc_no AS (
     SELECT
     ii2.instance_id AS instance_id,
@@ -230,20 +230,20 @@ WITH oclc_no AS (
         WHEN oclcno.oclc_number2 LIKE '(OCoLC)%' THEN SUBSTRING(oclcno.oclc_number2, 8)
         ELSE oclcno.oclc_number2 
         END AS oclc_no
-    FROM LOCAL.h_s_7 hsn 
+    FROM local_hathi.h_s_7 hsn 
     INNER JOIN oclc_no AS oclcno ON hsn.instance_id = oclcno.instance_id
 ;
-CREATE INDEX ON local.h_s_8 (instance_id);
-CREATE INDEX ON local.h_s_8 (instance_hrid);
-CREATE INDEX ON local.h_s_8 (call_number);
-CREATE INDEX ON local.h_s_8 (permanent_location_name);
-CREATE INDEX ON local.h_s_8 (id_type);
-CREATE INDEX ON local.h_s_8 (oclc_number2);
-CREATE INDEX ON local.h_s_8 (oclc_no);
+CREATE INDEX ON local_hathi.h_s_8 (instance_id);
+CREATE INDEX ON local_hathi.h_s_8 (instance_hrid);
+CREATE INDEX ON local_hathi.h_s_8 (call_number);
+CREATE INDEX ON local_hathi.h_s_8 (permanent_location_name);
+CREATE INDEX ON local_hathi.h_s_8 (id_type);
+CREATE INDEX ON local_hathi.h_s_8 (oclc_number2);
+CREATE INDEX ON local_hathi.h_s_8 (oclc_no);
 
 --9------------------------------selects issn number and value for government document from 008 --------------
-DROP TABLE IF EXISTS LOCAL.h_s_final;
-CREATE TABLE LOCAL.h_s_final AS
+DROP TABLE IF EXISTS local_hathi.h_s_final;
+CREATE TABLE local_hathi.h_s_final AS
 WITH gov_doc AS (
     SELECT
     sm.instance_hrid AS instance_hrid,
@@ -269,7 +269,7 @@ dist_hrid_select AS (
    SELECT 
    DISTINCT(ho.instance_hrid),
    ho.oclc_no AS oclc_no
-   FROM LOCAL.h_s_8 AS ho
+   FROM local_hathi.h_s_8 AS ho
 )
 SELECT
    ds.oclc_no,
@@ -281,7 +281,7 @@ SELECT
    LEFT JOIN issn_select AS issn ON ds.instance_hrid = issn.instance_hrid
    GROUP BY ds.oclc_no, ds.instance_hrid,  issn.issn_no, gd.gov_doc
 ;
-CREATE INDEX ON local.h_s_final (oclc_no);
-CREATE INDEX ON local.h_s_final (instance_hrid);
-CREATE INDEX ON local.h_s_final (issn_no);
-CREATE INDEX ON local.h_s_final (gov_doc);
+CREATE INDEX ON local_hathi.h_s_final (oclc_no);
+CREATE INDEX ON local_hathi.h_s_final (instance_hrid);
+CREATE INDEX ON local_hathi.h_s_final (issn_no);
+CREATE INDEX ON local_hathi.h_s_final (gov_doc);
