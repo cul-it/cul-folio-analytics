2ae05525d958272354684201b9e02a5f71c66131
Linda Miller <69207020+LM-15@users.noreply.github.com>
2022-05-11 16:32:22 -0400

    Update title_count_servremo_adc_stats.sql

--- canned_reports/CR164/title_count_servremo_adc_stats.sql
+++ canned_reports/CR164/title_count_servremo_adc_stats.sql
@@ -1,23 +1,29 @@
+ --CR164 title_count_servremo_adc_stats_May2022 ~7 minutes
+--updates made: added sr.state = 'ACTUAL'; added names of the other sets of title queries used;
+--added VACUUM ANALYZE; added sql to remove unpurchased pda/dda.
+ 
 /* This set of queries pulls counts of titles with holdings locations of serv,remo. Includes any
-  * remote electronic resources cataloged on print records.
+ * remote electronic resources cataloged on print records.
  * 
  * A&P uses this and two other sets of queries to get the title counts needed for ARL, ACRL 
- * and NCES reporting. ARL asks for one title count (all formats). ACRL asks for title counts 
- * by bib format, and by electronic vs. physcial. For ACRL, all serials are counted as serials, 
+ * and NCES reporting (CR162 for titles with physical holdings - excluding microform holdings; and
+ * CR163 for microform holdings). ARL asks for one title count (all formats). ACRL asks for title 
+ * counts by bib format, and by electronic vs. physcial. For ACRL, all serials are counted as serials, 
  * and any non-serial microforms are counted as media. Duplication between formats is to be included.
  * 
  *The first 3 queries create local data tables stored in the "local_statistics" schema. The results
  *of the third query are used by the fourth query to a count of remote e-resources titles by 
- *translated bib format. 
+ *translated bib format. The fourth query also removes known unpurchased PDA/DDA items via a subquery.
  * 
  *Filtering requiring updates as needed:
- *Query 2: locations to be excluded (review as needed with CUL)
- *Query 4: update the bib_fmt_and_location_trans_table_csv table (review as needed)
+ *Query 2: locations to be excluded (review as needed with LTS and unit libraries)
+ *Query 4: update the bib_fmt_and_location_trans_table_csv table (review as needed with LTS)
+ *Query 4: list of unpurchased PDA/DDA 899 codes to be excluded (review as needed; current expert: You Lee Chun)
  */
 
  /* Query 1: this query pulls all unspressed records, with leader bib format type from 000 and 
   * date created.*/
--- ~5 min; 8,705,395 Records on 3/3/22
+-- ~5 min; 8,679,670 Records on 5/10/22
 DROP TABLE IF EXISTS LOCAL_statistics.titlservr_ct_1; 
 CREATE TABLE local_statistics.titlservr_ct_1 AS
 SELECT DISTINCT 
@@ -29,8 +35,10 @@ SELECT DISTINCT
     ie.record_created_date::date AS date_created
     FROM srs_marctab sm 
     LEFT JOIN folio_reporting.instance_ext AS ie ON sm.instance_id = ie.instance_id
+    LEFT JOIN srs_records sr ON sm.srs_id = sr.id
     WHERE sm.field LIKE '000'
     AND (ie.discovery_suppress = 'FALSE' OR ie.discovery_suppress IS NULL)
+    AND sr.state  = 'ACTUAL'
 ;
 CREATE INDEX ON local_statistics.titlservr_ct_1(instance_hrid);
 CREATE INDEX ON local_statistics.titlservr_ct_1(instance_id);
@@ -38,11 +46,12 @@ CREATE INDEX ON local_statistics.titlservr_ct_1(field);
 CREATE INDEX ON local_statistics.titlservr_ct_1("format_type");
 CREATE INDEX ON local_statistics.titlservr_ct_1(discovery_suppress);
 CREATE INDEX ON local_statistics.titlservr_ct_1(date_created);
+VACUUM ANALYZE local_statistics.titlservr_ct_1;
 
 /*Query 2: Adds holdings information and removes records with locations and statuses not wanted.
  * Gets records with serv,remo locations only (excluding microforms).
  * Holdings records must be unsuppressed*/
--- 1 min; 2,522,383 records on 3/3/22
+-- 1 min; 2,523,159 records on 4/15/22;  2,467,907 on 5/10/22
 DROP TABLE IF EXISTS local_statistics.titlservr_ct_2; 
 CREATE TABLE local_statistics.titlservr_ct_2 AS 
 SELECT DISTINCT 
@@ -85,7 +94,7 @@ SELECT DISTINCT
     AND h.permanent_location_name NOT ilike 'z-test location'
     AND h.call_number !~~* 'on order%'
     AND h.call_number !~~* 'in process%'
-    AND h.call_number !~~* 'Available for the library to purchase'
+    AND h.call_number !~~* 'Available for the library to purchase' 
     AND h.call_number !~~* '%film%' 
     AND h.call_number !~~* '%fiche%'
     AND h.call_number !~~* '%micro%'
@@ -103,10 +112,11 @@ CREATE INDEX ON local_statistics.titlservr_ct_2 (permanent_location_name);
 CREATE INDEX ON local_statistics.titlservr_ct_2 (call_number);
 CREATE INDEX ON local_statistics.titlservr_ct_2 (discovery_suppress);
 CREATE INDEX ON local_statistics.titlservr_ct_2 (date_created);
+VACUUM ANALYZE local_statistics.titlservr_ct_2;
 
 
 /*Query 3: To make title count unique again.*/
--- 1 minute; 2,522,363 rows on 3/3/22 
+-- 1 minute; 2,523,139 rows on 4/15/22; 2,467,887 on 5/10/22
 DROP TABLE IF EXISTS local_statistics.titlservr_ct_3; 
 CREATE TABLE LOCAL_statistics.titlservr_ct_3 AS 
 SELECT distinct
@@ -116,15 +126,100 @@ FROM local_statistics.titlservr_ct_2 ft
 ;
 CREATE INDEX ON local_statistics.titlservr_ct_3 ("format_type");
 CREATE INDEX ON local_statistics.titlservr_ct_3 (instance_hrid);
+VACUUM ANALYZE local_statistics.titlservr_ct_3;
 
-/*Query 4: Groups and counts titles in titlservr_ct_3 by format, adding format translation.*/
--- 1 minute; 46 rows group 2,522,363 titles on 3/3/22
-SELECT distinct
+--Query 4: Groups and counts titles in titlservr_ct_3 by format, adding format TRANSLATION. Also removes
+--known unpurchased PDA/DDA items through a subquery.*/
+-- 1 minute; 46 rows group 2,516,100 titles on 4/15/22; 2,461,066 on 5/10/22
+WITH title_unpurch AS 
+(SELECT DISTINCT 
+	sm.instance_hrid,
+    sm.instance_id,
+    sm.field,
+    sm."content",
+    ie.discovery_suppress
+    FROM srs_marctab sm 
+    LEFT JOIN folio_reporting.instance_ext AS ie ON sm.instance_id = ie.instance_id
+    LEFT JOIN srs_records sr ON sm.srs_id = sr.id
+    WHERE sm.field LIKE '899'
+    AND sm.sf LIKE 'a'
+    AND ((sm."content" ILIKE 'DDA_pqecebks') 
+    OR (sm."content" ILIKE 'DDA_eastvwebksmu')
+    OR (sm."content" ILIKE ' DDA_eastvwebksmu')
+    OR (sm."content" ILIKE 'DDA_esatvwebksmu')
+    OR (sm."content" ILIKE 'DDA_casaliniebkmu')
+    OR (sm."content" ILIKE 'Appr_eastvwebks'))
+    --OR (sm."content" ILIKE 'Couttspdbappr'))
+    AND (ie.discovery_suppress = 'FALSE' OR ie.discovery_suppress IS NULL)
+    AND sr.state  = 'ACTUAL'
+)
+SELECT DISTINCT 
 tc3."format_type" AS "Bib Format",
 bft.bib_format_display,
 count(tc3.instance_hrid) AS "Total"
 FROM local_statistics.titlservr_ct_3 tc3
 LEFT JOIN local_statistics.bib_fmt_and_location_trans_tables_csv bft ON tc3."format_type" = bft.bib_format
+LEFT JOIN title_unpurch ON tc3.instance_hrid = title_unpurch.instance_hrid
+WHERE title_unpurch.instance_hrid IS NULL
 GROUP BY tc3."format_type", bft.bib_format_display
 ORDER BY bft.bib_format_display
-;
+; 
+
+
+
+/*
+
+
+--other testing for unpurchased. 4/4/22
+WITH title_unpurch AS 
+(SELECT DISTINCT 
+	sm.instance_hrid,
+    sm.instance_id,
+    sm.field,
+    sm."content",
+    ie.discovery_suppress
+    FROM srs_marctab sm 
+    LEFT JOIN folio_reporting.instance_ext AS ie ON sm.instance_id = ie.instance_id
+    WHERE sm.field LIKE '899'
+    AND (ie.discovery_suppress = 'FALSE' OR ie.discovery_suppress IS NULL)
+    AND ((sm."content" ILIKE '%dda%') 
+    OR (sm."content" ILIKE '%PDA%'))
+)
+
+
+  
+ 
+ 
+ --these are Natalya's 
+ SELECT 
+ie.holdings_hrid,
+ie.call_number,
+ie.permanent_location_name
+--count(ie.holdings_hrid)
+FROM folio_reporting.holdings_ext ie
+WHERE call_number ilike '%Available for the Library to Purchase%'
+--GROUP BY ie.holdings_hrid, ie.call_number;
+
+
+SELECT
+    --sm.instance_id,
+    --sm.instance_hrid,
+    --sm.field,
+    --sm.sf,
+    sm."content" AS unpurchased,
+    count (sm.instance_id) AS groupcount
+    --ie.call_number,
+    --ie.permanent_location_name
+    --i.discovery_suppress,
+    --sr.state
+FROM
+    srs_marctab sm
+    LEFT JOIN srs_records sr ON sm.srs_id = sr.id
+    LEFT JOIN inventory_instances i ON sm.instance_id = i.id 
+    LEFT JOIN folio_reporting.holdings_ext ie ON sm.instance_id=ie.instance_id 
+    WHERE (sm.field = '899' AND sm.sf = 'a' AND sm."content" iLIKE ('%DDA%')) -- FOR purchased
+    AND sr.state = 'ACTUAL' AND call_number IN ('No call number')
+    /*(sm."content" ilike ('%DDA%')FOR unpurchased */ 
+    GROUP BY unpurchased;
+    --GROUP BY sm.instance_hrid , sm."content", ie.call_number, ie.permanent_location_name;
+*/
