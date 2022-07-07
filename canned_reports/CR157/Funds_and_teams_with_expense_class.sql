c2913ada284f0b91af5840f86d829c35ee084d44
Vandana Shah <vp25@cornell.edu>
2022-07-07 15:47:01 -0400

    Update Funds_and_teams_with_expense_class.sql
    Updated on 7-7-2022, includes code changes for correct results when there is more than one fiscal year's data (includes FY matches on multiple fields).

--- canned_reports/CR157/Funds_and_teams_with_expense_class.sql
+++ canned_reports/CR157/Funds_and_teams_with_expense_class.sql
@@ -1,8 +1,13 @@
-WITH expense_class AS    
+WITH parameters AS (
+    SELECT
+        'FY2022' AS fiscal_year_code),
+expense_class AS    
 (SELECT 
-        fti.effective_fund_code,
+	ffy.code AS fiscal_year,       
+	fti.effective_fund_code,
         fti.effective_fund_name,
         fec.name AS expense_class_name,
+        ffy.code AS expense_class_fiscal_year_code,
         sum(case when ft.transaction_type = 'Credit' THEN fti.transaction_amount*-1 ELSE fti.transaction_amount END) as amount_spent_in_expense_class
 
 FROM folio_reporting.finance_transaction_invoices AS fti 
@@ -12,14 +17,18 @@ FROM folio_reporting.finance_transaction_invoices AS fti
         LEFT JOIN finance_expense_classes AS fec 
         ON fti.transaction_expense_class_id = fec.id
         
+        LEFT JOIN finance_fiscal_years AS ffy 
+        ON ffy.id = fti.transaction_fiscal_year_id   
+        WHERE ffy.code = (SELECT fiscal_year_code FROM parameters)
 GROUP BY 
         fti.effective_fund_code,
         fti.effective_fund_name,
-        fec.name
-        )
-
+        fec.name,
+        ffy.code
+)
 SELECT 
         to_char(current_date::DATE - 1,'mm/dd/yyyy') AS as_of_yesterdays_date,
+        fgffy.fiscal_year_id,
         ffy.code AS fiscal_year,
         fg.name AS team,
         fg.description AS fund_manager,
@@ -34,35 +43,23 @@ SELECT
         (fb.total_funding - fb.available) as total_spent_encumbered_or_awaiting_payment,
         fb.cash_balance,
         fb.available AS available_balance,
-        expense_class.expense_class_name,
-        expense_class.amount_spent_in_expense_class,
+        ec.expense_class_name,
+        ec.amount_spent_in_expense_class,
+        --ec.expense_class_fiscal_year_code,
         CASE 
                 WHEN fb.expenditures > 0                 
-                THEN expense_class.amount_spent_in_expense_class/fb.expenditures
+                THEN ec.amount_spent_in_expense_class/fb.expenditures
                 ELSE 0 
                 END AS percent_of_fund_spent_on_expense_class_to_date
-
-        
 FROM 
-        finance_group_fund_fiscal_years AS fgffy 
-        LEFT JOIN finance_groups AS fg                   
-        ON fgffy.group_id = fg.id
-        
-        LEFT JOIN finance_fiscal_years AS ffy
-        ON fgffy.fiscal_year_id = ffy.id
-        
-        LEFT JOIN finance_funds AS ff
-        ON fgffy.fund_id = ff.id
-        
-        LEFT JOIN expense_class 
-        ON ff.code = expense_class.effective_fund_code
-        
-        LEFT JOIN finance_budgets AS fb
-        ON ff.id = fb.fund_id
-        
-        LEFT JOIN finance_fund_types AS fft
-        ON ff.fund_type_id = fft.id
-
-
+	finance_funds AS ff     
+       LEFT JOIN finance_budgets AS fb ON fb.fund_id = ff.id  
+       LEFT JOIN finance_fiscal_years AS ffy ON fb.fiscal_year_id = ffy.id
+       LEFT JOIN finance_group_fund_fiscal_years AS fgffy  ON fgffy.budget_id = fb.id
+       LEFT JOIN finance_groups AS fg ON fg.id = fgffy.group_id
+       LEFT JOIN finance_fund_types AS fft ON ff.fund_type_id = fft.id
+       LEFT JOIN expense_class AS ec ON ff.code = ec.effective_fund_code     
+WHERE 
+	ffy.code = (SELECT fiscal_year_code FROM parameters)
 ORDER BY team, finance_funds_code, finance_funds_name, expense_class_name
 ;
