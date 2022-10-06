c7871d1a0dd0ed4f16cf3d0e13333d0b5b6ce2cf
Vandana Shah <vp25@cornell.edu>
2022-10-06 11:54:10 -0400

    Update ceased_or_cancelled_serials.sql
    This version runs much faster (a few seconds vs. 4 minutes) and does a better job of capturing ceased or cancelled titles .

--- canned_reports/CR128/ceased_or_cancelled_serials.sql
+++ canned_reports/CR128/ceased_or_cancelled_serials.sql
@@ -1,160 +1,141 @@
-WITH parameters AS (
-SELECT 
-'Olin'::varchar AS location_filter --enter the location name 
+WITH parameters AS 
+(SELECT
+        'Mann Library'::varchar AS library_filter, -- examples: 'Uris Library', 'Mathematics Library', 'Library Annex'
+        'TX%'::varchar AS lc_class_filter -- examples: 'T%', 'QA', 'NAC', 'NA%'
 ),
 
-inst AS
+-- 1. Get candidates
+
+data1 AS 
 (SELECT 
-       he.instance_id AS main_location_instance_id,
-       iext.instance_hrid,
-       he.holdings_id,
-       he.holdings_hrid,
-       he.call_number_prefix,
-       he.call_number AS main_location_call_no,
-       he.call_number_suffix,
-       he.type_name,
-       he.receipt_status
-       
-       FROM folio_reporting.holdings_ext AS he
-               LEFT JOIN folio_reporting.instance_ext as iext 
-               ON he.instance_id = iext.instance_id
+        ll.library_name,
+        he.permanent_location_name,
+        instext.title,
+        he.type_name as holdings_type_name,
+        instext.instance_id,
+        instext.instance_hrid,
+        invitems.hrid as item_hrid,
+        invitems.id as item_id,
+        he.holdings_id,
+        he.holdings_hrid,
+        he.call_number,
+        TRIM (CONCAT_WS (' ', he.call_number_prefix, he.call_number, he.call_number_suffix, 
+                CASE WHEN he.copy_number >'1' THEN concat ('c.', he.copy_number) ELSE '' END)) AS whole_call_number,
+        SUBSTRING (he.call_number,'^([a-zA-z]{1,3})') as lc_class,
+        he.receipt_status,
+        he.discovery_suppress AS holdings_suppress,
+        STRING_AGG (DISTINCT hs.statement,' | ') AS library_holdings_statements,
+        STRING_AGG (DISTINCT hn.note,' | ') AS library_holdings_note,
+        invitems.effective_shelving_order
+        
 
-       
-       WHERE he.permanent_location_name = (SELECT location_filter FROM parameters)
-       --and substring (he.call_number,1,3) like 'QA%'
-       AND he.type_name = 'Serial'
-           AND he.receipt_status = 'Not currently received'
-           AND he.call_number != ''
-       ),
-       
- nts AS
-(SELECT
-    he.holdings_id,
-    he.instance_id,
-    string_agg(DISTINCT nt."note", ' | ') AS holdings_notes
-    
-    FROM folio_reporting.holdings_notes AS nt
-    LEFT JOIN folio_reporting.holdings_ext AS he on he.holdings_id = nt.holdings_id
-    
-    GROUP BY he.holdings_id, he.instance_id
-    ),
-    
- statements AS 
- (SELECT
+FROM folio_reporting.instance_ext AS instext 
+        LEFT JOIN folio_reporting.holdings_ext AS he 
+        ON instext.instance_id = he.instance_id 
+        
+        LEFT JOIN folio_reporting.holdings_statements AS hs 
+        ON he.holdings_hrid = hs.holdings_hrid
+        
+        LEFT JOIN folio_reporting.holdings_notes AS hn 
+        ON he.holdings_id = hn.holdings_id
+        
+        LEFT JOIN inventory_items AS invitems 
+        ON invitems.holdings_record_id = he.holdings_id
+        
+        LEFT JOIN folio_reporting.locations_libraries AS ll 
+        ON he.permanent_location_id = ll.location_id
 
-        holdings_id,
-        string_agg(DISTINCT hs."statement", ' | ') AS holdings_summary
-        
-        FROM folio_reporting.holdings_statements AS hs 
-        
-        GROUP BY holdings_id
-        ),
-        
+WHERE 
+                (ll.library_name = (SELECT library_filter FROM parameters)
+                AND SUBSTRING (he.call_number,'^([a-zA-z]{1,3})') LIKE (SELECT lc_class_filter FROM parameters)
+                AND he.type_name IN ('Serial','Multi-part monograph')
+                AND (he.receipt_status ='Not currently received' OR he.receipt_status ILIKE '%Ceased%' OR hn.note similar to '%(cease|cancel)%'))
+                
+
+GROUP BY 
+        ll.library_name,
+        he.permanent_location_name,
+        instext.title,
+        instext.instance_id,
+        instext.instance_hrid,
+        invitems.hrid,
+        invitems.id,
+        he.holdings_id,
+        he.holdings_hrid,
+        he.call_number_prefix,
+        he.call_number,
+        he.call_number_suffix,
+        he.copy_number,
+        he.receipt_status,
+        he.type_name,
+        he.discovery_suppress
+),
+
+-- 2. Count the number of volumes for library holdings
+
 numvols AS 
- (SELECT
-       holdings_id,
+        (SELECT 
+                data1.holdings_hrid,
+                COUNT (data1.item_id) AS number_of_volumes
+        
+        FROM data1
+        GROUP BY data1.holdings_hrid
+),
 
-       count (ihi.item_id) AS number_of_volumes
-       
-        FROM folio_reporting.items_holdings_instances AS ihi
-       LEFT JOIN inventory_items AS ii 
-        ON ihi.item_id = ii.id
-       
-        GROUP BY holdings_id
-       ),
+-- 3. Find holdings at the Annex
 
-main AS 
-  (SELECT distinct
+annex AS 
+        (SELECT 
+                data1.instance_id,
+                STRING_AGG (DISTINCT he.permanent_location_name,' | ') AS annex_locations,
+                STRING_AGG (DISTINCT hs.statement, ' | ') AS annex_holdings
+                
+        FROM data1
+                LEFT JOIN folio_reporting.holdings_ext AS he 
+                ON data1.instance_id = he.instance_id 
+                
+                LEFT JOIN folio_reporting.holdings_statements AS hs 
+                ON he.holdings_id = hs.holdings_id
+                
+        WHERE he.permanent_location_name LIKE '%Annex%'
+        
+        GROUP BY data1.instance_id
+)
 
-       inst.call_number_prefix,
-       inst.main_location_call_no,
-       inst.call_number_suffix,
-       iext.instance_hrid,
-       he.holdings_hrid,
-       he.permanent_location_name,
-       he.call_number_prefix as other_call_no_prefix,
-       he.call_number AS other_location_call_number,
-       he.call_number_suffix as other_loc_suffix,
-       iext.title,
-       iext.discovery_suppress AS suppress_instance,
-       he.discovery_suppress AS suppress_holdings,
-       he.type_name AS holdings_type_name,
-       he.receipt_status,
-       statements.holdings_summary,     
-       nts.holdings_notes,      
-       numvols.number_of_volumes   
-              
-FROM 
-       inst           
-       left JOIN folio_reporting.instance_ext as iext ON inst.main_location_instance_id = iext.instance_id
-       left JOIN folio_reporting.holdings_ext as he ON iext.instance_id = he.instance_id
-       left JOIN public.inventory_holdings AS ih ON he.holdings_id = ih.id
-       LEFT JOIN statements ON ih.id = statements.holdings_id
-       LEFT JOIN nts ON he.holdings_id = nts.holdings_id
-       LEFT JOIN numvols ON he.holdings_id = numvols.holdings_id 
-       
-WHERE inst.main_location_call_no > ''
-       --and ie.discovery_suppress = 'false'
-       --and he.discovery_suppress = 'false'
-           AND (he.permanent_location_name = (SELECT location_filter FROM parameters) OR he.permanent_location_name LIKE '%Annex%')
+-- 4. Match the library's results to the Annex results and put everything in call number order
 
-ORDER BY 
-       inst.main_location_call_no, iext.instance_hrid, he.permanent_location_name, he.holdings_hrid, iext.title, he.call_number
-       ),
-
-main2 AS      
-(SELECT
-           main.call_number_prefix,
-       main.main_location_call_no,
-       main.call_number_suffix,
-       main.instance_hrid,
-       main.holdings_hrid,
-       ii.effective_shelving_order,
-       main.permanent_location_name,
-       main.other_call_no_prefix,
-       main.other_location_call_number,
-       main.other_loc_suffix,
-       main.title,
-       main.suppress_instance,
-       main.suppress_holdings,
-       main.holdings_type_name,
-       main.receipt_status,
-       main.holdings_summary,
-       main.holdings_notes,
-       main.number_of_volumes
-       
-FROM main 
-           LEFT JOIN folio_reporting.holdings_ext as he on he.holdings_hrid = main.holdings_hrid
-           LEFT JOIN folio_reporting.items_holdings_instances as ihi on he.holdings_id = ihi.holdings_record_id
-           LEFT JOIN inventory_items as ii on ihi.item_id = ii.id
-
-       
-ORDER BY 
-          main.main_location_call_no, ii.effective_shelving_order, main.instance_hrid, main.holdings_hrid
-          )
- 
-  SELECT DISTINCT
- 	   main2.call_number_prefix,
-       main2.main_location_call_no,
-       main2.call_number_suffix,
-       main2.instance_hrid,
-       main2.holdings_hrid,
-     --  ii.effective_shelving_order,
-       main2.permanent_location_name,
-       main2.other_call_no_prefix,
-       main2.other_location_call_number,
-       main2.other_loc_suffix,
-       main2.title,
-       main2.suppress_instance,
-       main2.suppress_holdings,
-       main2.holdings_type_name,
-       main2.receipt_status,
-       main2.holdings_summary,
-       main2.holdings_notes,
-       main2.number_of_volumes
- 
-    FROM main2
-    ;
+SELECT 
+                data1.library_name,
+                data1.permanent_location_name,
+                data1.title,
+                data1.holdings_type_name,
+                data1.instance_id,
+                data1.instance_hrid,
+                data1.holdings_hrid,
+                numvols.number_of_volumes,
+                data1.call_number,
+                data1.whole_call_number,
+                data1.lc_class,
+                data1.receipt_status,
+                data1.holdings_suppress,
+                data1.library_holdings_statements,
+                data1.library_holdings_note,
+                annex.annex_locations,
+                annex.annex_holdings,
+                data1.effective_shelving_order
+        
+        FROM data1 
+                LEFT JOIN annex 
+                ON data1.instance_id = annex.instance_id
+                
+                LEFT JOIN numvols 
+                ON data1.holdings_hrid = numvols.holdings_hrid
+                        
+        WHERE (data1.holdings_suppress = 'FALSE' OR data1.holdings_suppress IS NULL)
+                AND data1.whole_call_number NOT ILIKE '%thesis%'
+                
+        ORDER BY data1.effective_shelving_order COLLATE "C"
+;
 
 
      
