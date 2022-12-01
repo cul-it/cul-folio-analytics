b1215bf07f1034385dd2e6899824f76b40784e2e
Vandana Shah <vp25@cornell.edu>
2022-12-01 09:55:11 -0500

    Update BD_ILL_Counts.sql

--- canned_reports/CR174/BD_ILL_Counts.sql
+++ canned_reports/CR174/BD_ILL_Counts.sql
@@ -1,6 +1,6 @@
---CR174 BD/ILL loans and renewals counts from CUL to other universities
+--CR174
 --QUERY 1
-
+--BD/ILL loans and renewals counts of items loaned BY CUL to others (CUL is LENDER)
 
 WITH parameters AS (
     SELECT
@@ -13,12 +13,12 @@ loans AS
     li.loan_id,
     CASE WHEN li.renewal_count IS NULL THEN 0 ELSE li.renewal_count END AS renew_count,
     
-    CASE WHEN
-    	date_part ('month',li.loan_date ::DATE) >'6' 
+    CASE 
+	    WHEN date_part ('month',li.loan_date ::DATE) >'6' 
         THEN concat ('FY ', date_part ('year',li.loan_date::DATE) + 1) 
         ELSE concat ('FY ', date_part ('year',li.loan_date::DATE))
         END as fiscal_year_of_loan
-        
+     
         FROM folio_reporting.loans_items AS li
     )
 
@@ -41,7 +41,8 @@ WHERE
 	li.loan_date >= (SELECT start_date FROM parameters)
     and li.loan_date < (SELECT end_date FROM parameters)
     and (li.material_type_name not like 'BD%' and li.material_type_name not like '%ILL%')
-    and ((li.patron_group_name like 'Borrow%' or li.patron_group_name like 'Inter%') OR li.item_effective_location_name_at_check_out ILIKE ANY (ARRAY ['Borrow%', 'Inter%']))
+    and (li.patron_group_name like 'Borrow%' or li.patron_group_name like 'Inter%') 
+   
 
 GROUP BY
     TO_CHAR (CURRENT_DATE::DATE,'mm/dd/yyyy'),
@@ -56,8 +57,10 @@ ORDER BY
 ;
 
 
---CR174 BD/ILL – count of items borrowed from other universities
+
+--CR174
 --QUERY 2
+--BD/ILL – count of items borrowed BY CUL (CUL is BORROWER)
 
 WITH parameters AS (
     SELECT
@@ -75,8 +78,15 @@ SELECT
     	date_part ('month',li.loan_date ::DATE) >'6' 
         THEN concat ('FY ', date_part ('year',li.loan_date::DATE) + 1) 
         ELSE concat ('FY ', date_part ('year',li.loan_date::DATE))
-        END as fiscal_year_of_loan
-
+        END as fiscal_year_of_loan,
+        
+        CASE WHEN 
+        (li.material_type_name ilike 'BD%' OR li.item_effective_location_name_at_check_out ILIKE 'Borr%') THEN 'Borrow Direct'
+        WHEN (li.material_type_name ilike 'ILL*%' OR li.item_effective_location_name_at_check_out ILIKE 'Inter%') then 'Interlibrary Loan'
+		ELSE 'Not BDILL'
+		END AS BDILL_type
+		
+		
 FROM folio_reporting.loans_items AS li
         LEFT JOIN folio_reporting.locations_libraries AS ll 
         ON li.current_item_permanent_location_id = ll.location_id
@@ -84,16 +94,17 @@ FROM folio_reporting.loans_items AS li
 WHERE 
 		li.loan_date >= (SELECT start_date FROM parameters)
     	AND li.loan_date < (SELECT end_date FROM parameters) 
-        AND (li.current_item_permanent_location_name LIKE 'Borrow%' OR li.current_item_permanent_location_name LIKE 'Inter%')
+        AND (li.item_effective_location_name_at_check_out ILIKE ANY (ARRAY ['Borrow%', 'Inter%']) 
+        OR (li.material_type_name ILIKE ANY (ARRAY['BD%', 'ILL*%'])))
 
 GROUP BY
         TO_CHAR (CURRENT_DATE::DATE,'mm/dd/yyyy'),
         li.current_item_permanent_location_name,
         li.patron_group_name,
-        fiscal_year_of_loan
+        fiscal_year_of_loan,
+        BDILL_type
         
 ORDER BY
         li.current_item_permanent_location_name ASC,
         li.patron_group_name ASC
-        ;
-
+               ;
