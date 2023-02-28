cfe5e60119133b063bcc9c91ea11729f20e50d59
Vandana Shah <vp25@cornell.edu>
2023-02-28 13:37:36 -0500

    Create olin_microforms_with_circ_counts.sql

---
+++ ad_hoc_reports/AHR115/olin_microforms_with_circ_counts.sql
@@ -0,0 +1,4 @@
+AHR 115
+olin_microforms_with_circ_counts
+
+This is the second of three queries developed for the 2023 Olin Basement move project, and includes details of Olin and Kroch microforms, including bibliographic information and Voyager and Folio circ counts, to help selectors make decisions about moving or de-accessioning the materials. 
