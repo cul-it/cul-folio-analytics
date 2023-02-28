f1c56919e38a9252dd278d33288521ecb789b29b
Vandana Shah <vp25@cornell.edu>
2023-02-28 13:43:39 -0500

    Create olin_microforms_with_circs_and_srs_marctab_fields.sql

---
+++ ad_hoc_reports/AHR116/olin_microforms_with_circs_and_srs_marctab_fields.sql
@@ -0,0 +1,4 @@
+AHR 116
+Olin Microforms with Circs and SRS Marctab Fields.sql
+
+This is the third of three queries developed for the 2023 Olin Basement move project, and includes details of microform bibliographic information, srs marctab fields, and circ counts to help selectors make decisions about moving or de-accessioning the materials.
