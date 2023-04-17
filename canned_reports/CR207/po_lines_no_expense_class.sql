db2e24292d6ad94ea0597d98970a9d4b024847dc
AMCrowley <129436516+AMCrowley@users.noreply.github.com>
2023-04-17 15:51:51 -0400

    Create po_lines_no_expense_class.sql

---
+++ canned_reports/CR207/po_lines_no_expense_class.sql
@@ -0,0 +1,40 @@
+--CR207   
+--CR207
+--po_lines_no_expense_class
+--To get a listing of purchase order lines with no expense class selected when the PO was created
+--Includes fund code, purchase order line #,workflow status, order type, order format and Title
+-- This is pulling purchase order line information not tied to a transaction
+
+SELECT
+current_date AS current_date,
+PO_line_number AS po_line_number,
+ff.code AS fund_code,
+ppo.workflow_status AS workflow_status,
+ppo.order_type AS order_type,
+order_format AS order_format,
+title_or_package AS title_or_package
+
+FROM po_linesAS pol
+CROSS JOIN json_array_elements(json_extract_path(data, 'fundDistribution')) AS dist(data)
+LEFT JOIN finance_expense_classes AS fec ON json_extract_path_text(dist.DATA, 'expenseClassId') =fec.id
+LEFT JOIN finance_funds AS ff ON ff.id = json_extract_path_text(dist.data, 'fundId')
+LEFT JOIN po_purchase_orders AS ppo ON ppo.id = pol.purchase_order_id
+
+WHERE fec.name IS NULL
+AND ff.code != '522'
+AND ff.code != '999'
+AND ff.code != '9'
+AND ff.code != '7'
+AND ff.code != '3'
+AND ff.code != '4'
+AND ff.code != '70'
+AND ff.code != '6920'
+AND ff.code != '6921'
+AND ff.code != 'Look'
+AND ff.code != '515'
+AND ff.code != '518'
+AND ppo.workflow_status != 'Closed'
+
+ORDER BY
+pol.PO_line_number,
+ff.code;
