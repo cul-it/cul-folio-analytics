3a0a935b1e62d9897945bd5381ef0af66dd852d0
Sharon Beltaine <36549480+sbeltaine@users.noreply.github.com>
2023-06-28 15:54:34 -0400

    Update appr_invoices_bib_data.sql
    Joanne Leary updated instance_subject_extract subquery to work with Orchid release

--- canned_reports/CR134/appr_invoices_bib_data.sql
+++ canned_reports/CR134/appr_invoices_bib_data.sql
@@ -1,7 +1,9 @@
 --CR134 Approved Invoices with bib data
---This query provides the list of approved invoices within a date range along with vendor name, finance group name, 
+--This query provides the list of approved invoices within a date range along with vendor name, finance group name,
 --vendor invoice number, fund details, purchase order details, language, instance subject, fund type, expense class and bibliographic format.
---In cases where the quantity was incorrectly entered as zero, this query replaces zero with 1 
+--In cases where the quantity was incorrectly entered as zero, this query replaces zero with 1
+--6-28-23: updated instance_subject_extract subquery to work with Orchid release
+
 WITH parameters AS (
     SELECT
         '' AS payment_date_start_date,--enter invoice payment start date and end date in YYYY-MM-DD format
@@ -15,38 +17,40 @@ WITH parameters AS (
         ''::VARCHAR AS po_number,
         ''::VARCHAR AS format_name, -- Ex: Book, Serial, Textual Resource, etc.
         ''::VARCHAR AS expense_class-- Ex:Physical Res - one-time perpetual, One time Electronic Res - Perpetual etc.
-),   
+),  
 format_extract as (
-        SELECT 
+        SELECT
          sm.instance_id::uuid,
          substring(sm.content,7,2) AS bib_format_code,
          jl.bib_format_display
         FROM
         srs_marctab as sm
         LEFT JOIN local.jl_bib_format_display_csv AS jl ON substring(sm.content,7,2) = jl.bib_format
-        WHERE sm.field = '000' 
+        WHERE sm.field = '000'
 ),
+ 
 instance_subject_extract as (
-        SELECT
-        instances.id AS instance_id,
-        instances.hrid AS instance_hrid,
-        subjects.data #>> '{}' AS subject,
-        subjects.ordinality AS subject_ordinality
-        FROM
-        inventory_instances AS instances
-        CROSS JOIN LATERAL json_array_elements(json_extract_path(data, 'subjects'))
-        WITH ORDINALITY AS subjects (data)
-        WHERE subjects.ordinality = '1'
+                SELECT
+                    instances.id AS instance_id,
+                    instances.hrid AS instance_hrid,
+                    subjects.data #>> '{value}' AS subject,
+                    subjects.ordinality AS subject_ordinality
+                FROM
+                    inventory_instances AS instances
+                CROSS JOIN jsonb_array_elements((instances.data #> '{subjects}')::jsonb)
+                WITH ORDINALITY AS subjects (data)
+                WHERE subjects.ORDINALITY = '1'
 ),
-locations as 
+ 
+locations as
 (SELECT
     pol.id AS pol_id,
     json_extract_path_text(locations.data, 'quantity') AS pol_location_qty,
     json_extract_path_text(locations.data, 'quantityElectronic') AS pol_loc_qty_elec,
-    json_extract_path_text(locations.data, 'quantityPhysical') AS pol_loc_qty_phys,      
-    CASE WHEN json_extract_path_text(locations.data, 'locationId') IS NOT NULL THEN json_extract_path_text(locations.data, 'locationId') 
+    json_extract_path_text(locations.data, 'quantityPhysical') AS pol_loc_qty_phys,     
+    CASE WHEN json_extract_path_text(locations.data, 'locationId') IS NOT NULL THEN json_extract_path_text(locations.data, 'locationId')
          ELSE ih.permanent_location_id
-    END AS pol_location_id,      
+    END AS pol_location_id,     
     CASE WHEN il.name IS NOT NULL THEN il.name
          ELSE il2.name
     END AS pol_location_name,
@@ -66,14 +70,14 @@ pol_holdings_id AS (
         pol.id AS pol_id,
         json_extract_path_text(locations.data, 'locationId') AS pol_loc_id,
         json_extract_path_text(locations.data, 'holdingId') AS pol_holding_id
-
+ 
                 FROM
                po_lines AS pol
                CROSS JOIN json_array_elements(json_extract_path(data, 'locations')) AS locations (data)
 ),
   finance_transaction_invoices_ext AS (
         SELECT
-        fti.transaction_id AS transaction_id,      
+        fti.transaction_id AS transaction_id,     
         fti.invoice_date::date,
                 fti.invoice_payment_date::DATE AS invoice_payment_date,
                 fti.transaction_fiscal_year_id,
@@ -96,7 +100,7 @@ FROM
                 LEFT JOIN finance_funds AS ff ON ff.code = fti.effective_fund_code
                 LEFT JOIN finance_fiscal_years AS ffy ON ffy.id = fti.transaction_fiscal_year_id
                 LEFT JOIN finance_fund_types AS fft ON fft.id = ff.fund_type_id
-                LEFT JOIN finance_ledgers AS fl ON ff.ledger_id = fl.id               
+                LEFT JOIN finance_ledgers AS fl ON ff.ledger_id = fl.id              
 ),
 fund_fiscal_year_group AS (
 SELECT
@@ -107,27 +111,27 @@ SELECT
     fgffy.fiscal_year_id AS fund_fiscal_year_id,
     ffy.code AS fiscal_year_code
 FROM
-    finance_groups AS FG 
+    finance_groups AS FG
     LEFT JOIN finance_group_fund_fiscal_years AS FGFFY ON fg.id = fgffy.group_id
     LEFT JOIN finance_fiscal_years AS ffy ON ffy. id = fgffy.fiscal_year_id
     LEFT JOIN finance_funds AS FF ON FF.id = fgffy.fund_id
 WHERE ((ffy.code = (SELECT fiscal_year_code FROM parameters)) OR ((SELECT fiscal_year_code FROM parameters) = ''))
 ORDER BY ff.code
-
+ 
 ),
-new_quantity AS 
-        (SELECT 
+new_quantity AS
+        (SELECT
         id AS invoice_line_id,
        CASE WHEN quantity = 0
                 THEN 1
                 ELSE quantity
                         END AS fixed_quantity
-        FROM invoice_lines 
+        FROM invoice_lines
 )
 -- MAIN QUERY
 SELECT distinct
-        current_date AS current_date,           
-       CASE WHEN 
+        current_date AS current_date,          
+       CASE WHEN
                         ((SELECT
                         payment_date_start_date::varchar
                         FROM
@@ -139,10 +143,10 @@ SELECT distinct
                 THEN 'Not Selected'
                 ELSE
                         (SELECT payment_date_start_date::varchar
-                FROM parameters) || ' to '::varchar || 
+                FROM parameters) || ' to '::varchar ||
                 (SELECT payment_date_end_date::varchar
                                 FROM parameters)
-                        END AS payment_date_range,       
+                        END AS payment_date_range,      
        --ftie.transaction_id AS transaction_id,
        --iext.title AS instance_title,
        replace(replace (iext.title, chr(13), ''),chr(10),'') AS instance_title,--updated code to get rid of carriage returns
@@ -168,13 +172,13 @@ SELECT distinct
        ftie.effective_fund_code,
        ftie.fund_type_name,
        po.po_number,
-       pol.po_line_number,      
-       formatt.bib_format_display AS format_name,        
+       pol.po_line_number,     
+       formatt.bib_format_display AS format_name,       
        inssub.subject AS instance_subject, -- This IS the subject that is first on the list.
        lang.language AS LANGUAGE, -- This IS the language that is first on the list.
        --pol.title_or_package AS po_line_title_or_package,
        replace(replace (pol.title_or_package, chr(13), ''),chr(10),'') AS po_line_title_or_package,--updated code to get rid of carriage returns
-       fq.fixed_quantity AS quantity,       
+       fq.fixed_quantity AS quantity,      
        ftie.external_account_no
 FROM
         finance_transaction_invoices_ext AS ftie
@@ -199,13 +203,14 @@ WHERE
         AND ((ftie.fund_type_name = (SELECT fund_type FROM parameters)) OR ((SELECT fund_type FROM parameters) = ''))
         AND ((ffyg.finance_group_name = (SELECT transaction_finance_group_name FROM parameters)) OR ((SELECT transaction_finance_group_name FROM parameters) = ''))
         AND ((ftie.finance_ledger_name = (SELECT transaction_ledger_name FROM parameters)) OR ((SELECT transaction_ledger_name FROM parameters) = ''))
-        AND ((ftie.fiscal_year_code = (SELECT fiscal_year_code FROM parameters)) OR ((SELECT fiscal_year_code FROM parameters) = '')) 
+        AND ((ftie.fiscal_year_code = (SELECT fiscal_year_code FROM parameters)) OR ((SELECT fiscal_year_code FROM parameters) = ''))
         AND ((po.order_type = (SELECT order_type_filter FROM parameters)) OR ((SELECT order_type_filter FROM parameters) = ''))
         AND ((po.po_number = (SELECT po_number FROM parameters)) OR ((SELECT po_number FROM parameters) = ''))
         AND ((fec.name = (SELECT expense_class FROM parameters)) OR ((SELECT expense_class FROM parameters) = ''))
         AND (lang.language_ordinality = '1' OR lang.language_ordinality ISNULL)
-        AND ((formatt.bib_format_display= (SELECT format_name FROM parameters)) OR ((SELECT format_name FROM parameters) = ''))    
- GROUP BY
+        AND ((formatt.bib_format_display= (SELECT format_name FROM parameters)) OR ((SELECT format_name FROM parameters) = ''))
+ 
+GROUP BY
           ftie.transaction_id,
        iext.title,
        iext.instance_hrid,
@@ -227,14 +232,14 @@ WHERE
        ftie.effective_fund_code,
        ftie.fund_type_name,
        po.po_number,
-       pol.po_line_number,      
-       formatt.bib_format_display,        
-       inssub.subject, 
-       lang.language, 
+       pol.po_line_number,     
+       formatt.bib_format_display,       
+       inssub.subject,
+       lang.language,
        pol.title_or_package,
-       fq.fixed_quantity,       
-       ftie.external_account_no     
-ORDER BY 
+       fq.fixed_quantity,      
+       ftie.external_account_no    
+ORDER BY
         instance_title,
         ftie.finance_ledger_name,
         ffyg.finance_group_name,
@@ -242,5 +247,7 @@ ORDER BY
         ftie.invoice_vendor_name,
         inv.vendor_invoice_no,
         po.po_number,
-        pol.po_line_number 
+        pol.po_line_number
         ;
+ 
+
