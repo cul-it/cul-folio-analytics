96223345a06ad1db57f7b664193a6e9b7b35b69f
Natalya Pikulik <62571646+natalyapik@users.noreply.github.com>
2024-03-08 14:32:29 -0500

    Create inst_updated_date.sql

---
+++ metadb/canned_reports/MCR232/inst_updated_date.sql
@@ -0,0 +1,14 @@
+SELECT
+  sr.instance_hrid,
+  sr.field,
+  sr.CONTENT,
+  i.creation_date::date, -- changed from record_created_date IN ldp
+  i.__current,
+  jsonb_extract_path_text(i.jsonb, 'metadata', 'updatedByUserId') AS user_uuid,
+  jsonb_extract_path_text(i.jsonb, 'metadata', 'updatedDate')::date AS updated_date
+FROM folio_source_record.marc__t sr
+LEFT JOIN folio_inventory.instance__ as i ON sr.instance_id = i.id
+WHERE (sr.field ='245' AND sr.sf='a')
+  AND jsonb_extract_path_text(i.jsonb, 'metadata', 'updatedDate') > '2023-01-1'
+LIMIT 10
+  ;
