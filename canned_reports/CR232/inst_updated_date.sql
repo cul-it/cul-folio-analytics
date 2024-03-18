e8c6dc14ac58dbe815edd36dc119701a02ccf0c2
Sharon Markus <36549480+sbeltaine@users.noreply.github.com>
2024-03-18 13:20:25 -0400

    Update inst_updated_date.sql

--- canned_reports/CR232/inst_updated_date.sql
+++ canned_reports/CR232/inst_updated_date.sql
@@ -1,7 +1,11 @@
 --CR232
 --inst_updated_date
---last updated
-
+--created 3-8-24
+--query written by Natalya Pikulik
+--This query pulls the updated_by_userid and the updated_date field data from 
+--the inventory instance record using the instance_ext derived table. 
+--This data is then joined to the MARC 245/a field to get the instance title 
+--and instance HRID. Please use the LIMIT shown or take it out, depending on your need.
     
 SELECT
     sr.instance_hrid,
