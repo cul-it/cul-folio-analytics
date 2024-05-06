be77776e37fe3adbfedbec49db4262bd7f1a4b37
Sharon Markus <36549480+sbeltaine@users.noreply.github.com>
2024-05-06 13:07:24 -0400

    Update daily_appr_inv_vendor.sql

--- metadb/canned_reports/MCR114/daily_appr_inv_vendor.sql
+++ metadb/canned_reports/MCR114/daily_appr_inv_vendor.sql
@@ -1,9 +1,8 @@
--- MCR114
--- daily_appr_inv_vendor.sql
--- written by Nancy Bolduc, revised for Metadb by Sharon Markus, reviewed and tested by 
--- Last updated 2/19/24
-
--- This query provides the list of invoices paid by vendor along with voucher lines details.
+--MCR114
+--daily_appr_inv_vendor
+--This query provide the list of invoices paid by vendor along with voucher lines details.
+--written by Nancy Bolduc, revised for Metadb by Sharon Markus, and tested by Ann Crowley
+--last updated 5/6/24
 
 WITH parameters AS (
     SELECT
@@ -12,42 +11,42 @@ WITH parameters AS (
         '2022-06-30' :: DATE AS voucher_date_end_date -- Excludes the selected date
 )
 SELECT 
-	(SELECT
-		voucher_date_start_date::VARCHAR
-     FROM
-        parameters) || ' to '::VARCHAR || (
-     SELECT
-        voucher_date_end_date::VARCHAR
-     FROM
-        parameters) AS date_range,	
+		(SELECT
+			voucher_date_start_date::varchar
+     	FROM
+        	parameters) || ' to '::varchar || (
+     	SELECT
+        	voucher_date_end_date::varchar
+     	FROM
+        	parameters) AS date_range,	
     org.erp_code AS vendor_erp_code,
-    org.name AS vendor_name,
-    inv.vendor_invoice_no,
-    invv.voucher_number,
-    invvl.external_account_number,
-    invv.export_to_accounting AS export_to_accounting,
-    invvl.amount
+	org.name AS vendor_name,
+	inv.vendor_invoice_no,
+	invv.voucher_number,
+	invvl.external_account_number,
+	invv.export_to_accounting AS export_to_accounting,
+	invvl.amount
 FROM 
-    folio_invoice.voucher_lines__t AS invvl 
-    LEFT JOIN folio_derived.invoice_voucher_lines_fund_distributions AS invvlfd ON invvlfd.invoice_voucher_line_id = invvl.id
-    LEFT JOIN folio_invoice.invoice_lines__t AS invl ON invl.id = invvlfd.fund_distribution_invl_id
-    LEFT JOIN folio_invoice.vouchers__t AS invv ON invv.id = invvl.voucher_id
-    LEFT JOIN folio_invoice.invoices__t AS inv ON inv.id = invv.invoice_id
-    LEFT JOIN folio_orders.po_line__t AS pol ON pol.id = invl.po_line_id
-    LEFT JOIN folio_derived.po_instance AS poins ON poins.po_line_number = pol.PO_LINE_NUMBER 
-    LEFT JOIN folio_organizations.organizations__t AS org ON org.id = invv.vendor_id
+	folio_invoice.voucher_lines__t AS invvl 
+	LEFT JOIN folio_derived.invoice_voucher_lines_fund_distributions AS invvlfd
+	  ON invvlfd.invoice_voucher_line_id = invvl.id
+	LEFT JOIN folio_invoice.invoice_lines__t AS invl ON invl.id = invvlfd.fund_distribution_invl_id
+	LEFT JOIN folio_invoice.vouchers__t AS invv ON invv.id = invvl.voucher_id
+	LEFT JOIN folio_invoice.invoices__t AS inv ON inv.id = invv.invoice_id
+	LEFT JOIN folio_orders.po_line__t AS pol ON pol.id = invl.po_line_id
+	LEFT JOIN folio_derived.po_instance AS poins ON poins.po_line_number = pol.po_line_number 
+	LEFT JOIN folio_organizations.organizations__t AS org ON org.id = invv.vendor_id
 WHERE
-    (invv.voucher_date::DATE >= (SELECT voucher_date_start_date FROM parameters)) 
-    AND (invv.voucher_date::DATE < (SELECT voucher_date_end_date FROM parameters))
-    AND inv.status LIKE 'Paid'
+	(invv.voucher_date::date >= (SELECT voucher_date_start_date FROM parameters)) 
+	AND (invv.voucher_date::date < (SELECT voucher_date_end_date FROM parameters))
+	AND inv.status LIKE 'Paid'
 GROUP BY 
-    vendor_invoice_no,
+	vendor_invoice_no,
     org.erp_code,
-    org.name,
-    invvl.external_account_number,
-    invv.voucher_number,
-    invv.export_to_accounting,
-    invvl.amount
-ORDER BY vendor_name
-;
+	org.name,
+	invvl.amount,
+	invv.export_to_accounting,
+	invvl.external_account_number,
+	invv.voucher_number
+ORDER BY vendor_name;
 
