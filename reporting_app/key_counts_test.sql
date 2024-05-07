26427f52e11ec5cb1159a172c9118876057134d4
Sharon Markus <36549480+sbeltaine@users.noreply.github.com>
2024-05-07 16:12:10 -0400

    Update and rename get_count_user_group.json to key_counts_test.sql

---
+++ reporting_app/key_counts_test.sql
@@ -0,0 +1,25 @@
+--key_counts_test.sql
+SELECT  
+		to_char (now()::timestamp,'mm/dd/yyyy hh:mi am') as date_time,	
+		'ldp_cornell' AS DATABASE, 	
+(
+        SELECT COUNT(id)
+        FROM   inventory_instances
+        ) AS instances_from_inventory_instances,
+        (
+        SELECT COUNT(id)
+        FROM   inventory_holdings
+        ) AS holdings_from_inventory_holdings,
+        (
+        SELECT COUNT(loan_id)
+        FROM folio_reporting.loans_items
+        ) AS loans_from_loan_items,
+		(
+        SELECT COUNT(id)
+        FROM circulation_loans
+        ) AS loans_from_circulation_loans,
+		(
+        SELECT COUNT(id)
+        FROM   inventory_items
+        ) AS items_from_inventory_items
+        ;
