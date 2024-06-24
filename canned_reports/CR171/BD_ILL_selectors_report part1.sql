faacfbeceef921ca1650b04b9e50acc910b721fa
Vandana Shah <vp25@cornell.edu>
2024-06-24 10:56:00 -0400

    Update BD_ILL_selectors_report part1.sql
    Replaced criteria using item locations (permanent or effective) with holdings permanent location name

--- canned_reports/CR171/BD_ILL_selectors_report part1.sql
+++ canned_reports/CR171/BD_ILL_selectors_report part1.sql
@@ -1,9 +1,10 @@
 -- Lists title, patron group and department (where available) for items borrowed from other universities on Borrow Direct and Interlibrary Loan. Parameters include date range for the loans. 
 -- 6-7-24: updated to incorporate circ snapshot data
- 
+-- 6-24-24: replaced criteria using item locations (permanent or effective) with holdings permanent location name
+
 WITH parameters AS (
     SELECT
-        /* Choose a start and end date for the loans period */
+        /* Choose a start and end date for the loan period */
         '2023-07-01'::date AS start_date,
         '2024-06-30'::date AS end_date
         )
@@ -22,9 +23,9 @@ SELECT
        li.loan_policy_name,
        he.permanent_location_name,
        li.patron_group_name,
-       coalesce (udu.department_name, cs.custom_fields__department) as department_name,
-       coalesce (udu.department_code, cs.department_code) as department_code
- 
+       COALESCE (udu.department_name, cs.custom_fields__department) AS department_name,
+       COALESCE (udu.department_code, cs.department_code) AS department_code
+
 FROM folio_reporting.loans_items AS li 
        LEFT JOIN folio_reporting.holdings_ext AS he 
        ON li.holdings_record_id = he.holdings_id
@@ -38,15 +39,16 @@ FROM folio_reporting.loans_items AS li
        LEFT JOIN folio_reporting.users_departments_unpacked AS udu 
        ON li.user_id = udu.user_id
        
-       left join local_core.circ_snapshot4 cs 
-       on li.loan_id = cs.loan_id
- 
-        WHERE
-                li.loan_date >= (SELECT start_date FROM parameters)
+       LEFT JOIN local_core.circ_snapshot4 cs 
+       ON li.loan_id = cs.loan_id
+
+WHERE
+        li.loan_date >= (SELECT start_date FROM parameters)
         AND li.loan_date < (SELECT end_date FROM parameters)
-       AND (li.material_type_name like 'BD%' OR li.material_type_name like 'ILL%')
-       AND (li.current_item_permanent_location_name LIKE 'Borr%' OR li.current_item_permanent_location_name LIKE 'Int%')
+       AND (li.material_type_name LIKE 'BD%' OR li.material_type_name LIKE 'ILL%')
+       AND he.permanent_location_name SIMILAR TO '(Borr|Int)%'
        AND (udu.department_ordinality = 1 OR udu.department_ordinality IS NULL)
        
 ORDER BY title, loan_date
 ;
+
