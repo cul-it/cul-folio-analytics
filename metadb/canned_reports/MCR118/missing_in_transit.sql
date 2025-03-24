32f652ff98bbe35163e59893bac6fe3300eb4996
Sharon Markus <36549480+sbeltaine@users.noreply.github.com>
2025-03-24 14:40:49 -0400

    Update missing_in_transit.sql

--- metadb/canned_reports/MCR118/missing_in_transit.sql
+++ metadb/canned_reports/MCR118/missing_in_transit.sql
@@ -1,147 +1,148 @@
 --MCR118
 --missing_in_transit
---This is a revised version of the LDP query CR118 . It finds items that are still in transit after 10 days. This does not use the derived table "users_groups" because that table is wrong.
---Original query written by Joanne Leary (jl41)
---Query ported to Metadb by Joanne Leary (jl41)
---Query reviewers: Linda Miller (lm15), Vandana Shah(vp25)
+--last updated: 3-20-25
+--This query finds items that are still in transit after 10 days and shows requesters and designated pickup locations.
+--Written by: Joanne Leary (jl41)
+--Reviewed by: Linda Miller (lm15), Vandana Shah(vp25)
 --Date posted: 6/7/24
 
 WITH parameters AS 
 (SELECT 
-'%%'::VARCHAR AS library_name_filter,
-'In transit'::VARCHAR AS item_status_name_filter
+'%%'::varchar AS library_name_filter 
 ),
 
-intransit_items AS 
-(SELECT 
-	ll.library_name,
-	ie.item_id,
-	ie.item_hrid,
-	ie.barcode,
-	ihi.index_title AS title,
-	ie.effective_location_name,
-	TRIM (CONCAT (ie.effective_call_number_prefix,' ',ie.effective_call_number,' ',ie.effective_call_number_suffix,' ',ie.enumeration,' ',ie.chronology,
-		CASE WHEN ie.copy_number >'1' THEN concat ('c.',ie.copy_number) ELSE '' END)) AS call_number,
-	ie.in_transit_destination_service_point_name,
-	ie.status_name,
-	ie.status_date,
-	ri.request_date::DATE,
-	ri.request_type,
-	ri.request_status,
-	jsonb_extract_path_text(users.jsonb, 'personal', 'lastName') AS requester_last_name,
-	jsonb_extract_path_text(users.jsonb, 'personal', 'firstName') AS requester_first_name,
-	jsonb_extract_path_text(users.jsonb, 'username') AS requester_net_id,
-	jsonb_extract_path_text(users.jsonb, 'barcode') AS requester_barcode,
-	jsonb_extract_path_text(users.jsonb, 'active') AS requester_status,
-	jsonb_extract_path_text(users.jsonb, 'personal', 'email') AS requester_email,
-	item__t.effective_shelving_order
+-- 1. Get MAX request date for open in-transit requests
 
-FROM folio_derived.item_ext AS ie 
-	INNER JOIN folio_derived.items_holdings_instances AS ihi 
-	ON ie.item_id = ihi.item_id
+requests1 AS 
+(SELECT
+	request__t.item_id,
+	MAX (request__t.request_date)::DATE AS max_request_date,
+	request__t.status
 	
-	LEFT JOIN folio_derived.requests_items AS ri 
-	ON ie.item_id = ri.item_id
+	FROM folio_circulation.request__t 
+	WHERE request__t.status = 'Open - In transit'
 	
-	LEFT JOIN folio_users.users
-	ON ri.requester_id = users.id
-	
-	INNER JOIN folio_inventory.item__t 
-	ON ie.item_id = item__t.id
-	
-	INNER JOIN folio_derived.locations_libraries AS ll 
-	ON ie.effective_location_id = ll.location_id
-
-WHERE ie.status_name = (SELECT item_status_name_filter FROM parameters)
-	AND ((SELECT library_name_filter FROM parameters) = '' OR ll.library_name ILIKE (SELECT library_name_filter FROM parameters))
-	AND (ri.request_status LIKE 'Open%' OR ri.request_status IS NULL)
+	GROUP BY request__t.item_id, request__t.status
 ),
 
+-- 2. Join results to requests__t table and users table to get name of requester, requester status and pickup service point name; select only active users
 
-loc1 AS 
+requests2 AS 
 (SELECT 
-	intransit_items.library_name,
-	intransit_items.item_id,
-	intransit_items.item_hrid,
-	intransit_items.barcode,
-	intransit_items.title,
-	intransit_items.effective_location_name,
-	intransit_items.call_number,
-	intransit_items.in_transit_destination_service_point_name,
-	intransit_items.status_name,
-	intransit_items.status_date,
-	intransit_items.request_date::DATE,
-	intransit_items.request_type,
-	intransit_items.request_status,
-	intransit_items.requester_last_name,
-	intransit_items.requester_first_name,
-	intransit_items.requester_barcode,
-	intransit_items.requester_status,
-	intransit_items.requester_email,
-	intransit_items.effective_shelving_order,
-	MAX (check_in__t.occurred_date_time) AS most_recent_check_in
+	requests1.item_id,
+	requests1.max_request_date,
+	users.jsonb#>>'{personal,firstName}' AS requester_first_name,
+	users.jsonb#>>'{personal,lastName}' AS requester_last_name,
+	users.jsonb#>>'{username}' AS netid,
+	users.jsonb#>>'{active}' AS requester_status,
+	service_point__t.name AS pickup_service_point_name,
+	requests1.status AS request_status,
+	current_date::DATE - requests1.max_request_date AS days_since_requested
 
-FROM intransit_items
-	INNER JOIN folio_circulation.check_in__t
-	ON intransit_items.item_id = check_in__t.item_id 
+FROM requests1 
+	INNER JOIN folio_circulation.request__t 
+	ON requests1.item_id = request__t.item_id 
+		AND requests1.max_request_date = request__t.request_date::DATE
+	
+	INNER JOIN folio_users.users 
+	ON request__t.requester_id = users.id 
+	
+	INNER JOIN folio_inventory.service_point__t 
+	ON request__t.pickup_service_point_id = service_point__t.id
 
-GROUP BY 
-	intransit_items.library_name,
-	intransit_items.item_id,
-	intransit_items.item_hrid,
-	intransit_items.barcode,
-	intransit_items.title,
-	intransit_items.effective_location_name,
-	intransit_items.call_number,
-	intransit_items.in_transit_destination_service_point_name,
-	intransit_items.status_name,
-	intransit_items.status_date,
-	intransit_items.request_date::DATE,
-	intransit_items.request_type,
-	intransit_items.request_status,
-	intransit_items.requester_last_name,
-	intransit_items.requester_first_name,
-	intransit_items.requester_barcode,
-	intransit_items.requester_email,
-	intransit_items.requester_status,
-	intransit_items.effective_shelving_order
+WHERE (users.jsonb#>>'{active}')::boolean = true
+),
+
+-- 3. Get items with an "Open - In transit" status; get last check-in date and location; join to requests
+
+recs AS 
+(SELECT 
+	loclibrary__t.name AS library_name,
+	location__t.name AS holdings_location_name,
+	instance__t.title,
+	TRIM (CONCAT (item.jsonb#>>'{effectiveCallNumberComponents,prefix}',' ',
+		item.jsonb#>>'{effectiveCallNumberComponents,callNumber}',' ',
+		item.jsonb#>>'{effectiveCallNumberComponents,suffix}',' ',
+		item.jsonb#>>'{enumeration}',' ',
+		item.jsonb#>>'{chronology}',
+		CASE 
+			WHEN item.jsonb#>>'{copyNumber}' >'1' 
+			THEN CONCAT ('c.',item.jsonb#>>'{copyNumber}') 
+			ELSE NULL END)) AS item_call_number,
+	item.jsonb#>>'{barcode}' AS item_barcode,
+	instance__t.hrid AS instance_hrid,
+	hrt.hrid AS holdings_hrid,
+	item.jsonb#>>'{hrid}' AS item_hrid,	
+	item.jsonb#>>'{status,name}' AS item_status_name,
+	(item.jsonb#>>'{status,date}')::DATE AS item_status_date,
+	service_point__t.name AS last_checkin_location,
+	to_char ((item.jsonb#>>'{lastCheckIn,dateTime}')::TIMESTAMP,'mm-dd-yyyy hh:mi am') AS last_checkin_date,
+	requests2.requester_last_name,
+	requests2.requester_first_name,
+	requests2.netid,
+	requests2.max_request_date AS request_date,
+	requests2.pickup_service_point_name,
+	requests2.request_status,
+	requests2.days_since_requested,
+	item.jsonb#>>'{effectiveShelvingOrder}' AS shelving_order
+
+FROM folio_inventory.instance__t 
+	LEFT JOIN folio_inventory.holdings_record__t AS hrt 
+	ON instance__t.id = hrt.instance_id 
+	
+	LEFT JOIN folio_inventory.item 
+	ON hrt.id = (item.jsonb#>>'{holdingsRecordId}')::uuid
+	
+	LEFT JOIN folio_inventory.location__t 
+	ON hrt.permanent_location_id = location__t.id 
+	
+	LEFT JOIN folio_inventory.loclibrary__t 
+	ON location__t.library_id = loclibrary__t.id 
+	
+	LEFT JOIN folio_inventory.service_point__t 
+	ON (item.jsonb#>>'{lastCheckIn,servicePointId}')::UUID = service_point__t.id
+	
+	LEFT JOIN requests2
+	ON item.id = requests2.item_id
+	
+
+WHERE (loclibrary__t.name ILIKE (SELECT library_name_filter FROM parameters) OR (SELECT library_name_filter FROM parameters) ='') 
+	AND item.jsonb#>>'{status,name}' = 'In transit'
+	AND (instance__t.discovery_suppress = false OR instance__t.discovery_suppress IS NULL)
+	AND (hrt.discovery_suppress = false OR hrt.discovery_suppress IS NULL)
 )
 
+-- 4. Get size based on the presence of plusses in the whole item call number; select items that have been in transit for more than 10 days; sort by library, location, size and call number
+
 SELECT
-	TO_CHAR (CURRENT_DATE::date,'mm/dd/yyyy') AS todays_date,	
-	loc1.library_name,
-	loc1.effective_location_name,
-	loc1.title,
-	loc1.call_number,
-	loc1.item_hrid,
-	loc1.barcode,
-	loc1.status_name,
-	loc1.status_date::DATE,
-	loc1.most_recent_check_in::DATE,
-	service_point__t.name AS discharge_service_point_name,
-	loc1.in_transit_destination_service_point_name,
-	CURRENT_DATE::DATE - loc1.most_recent_check_in::DATE AS days_in_transit,
-	loc1.request_date::DATE,
-	loc1.request_type,
-	loc1.request_status,
-	loc1.requester_last_name,
-	loc1.requester_first_name,
-	loc1.requester_barcode,
-	loc1.requester_email,
-	CASE 
-		WHEN loc1.requester_status IS NULL THEN NULL 
-		WHEN loc1.requester_status = 'true' THEN 'Active' 
-		ELSE 'Inactive' END AS requester_status
+	recs.library_name,
+	recs.holdings_location_name,
+	recs.title,
+	CASE
+		WHEN recs.item_call_number LIKE '%+++%' THEN '+++'
+	    WHEN recs.item_call_number LIKE '%++%' THEN '++'
+	    WHEN recs.item_call_number LIKE '%+%' THEN '+'
+	    ELSE ''
+	    END AS size_group,
+	recs.item_call_number,
+	recs.item_barcode,
+	recs.instance_hrid,
+	recs.holdings_hrid,
+	recs.item_hrid,
+	recs.item_status_name,
+	recs.item_status_date,
+	recs.last_checkin_location,
+	recs.last_checkin_date,
+	recs.requester_last_name,
+	recs.requester_first_name,
+	recs.netid,
+	recs.request_date,
+	recs.pickup_service_point_name,
+	recs.request_status,
+	recs.days_since_requested
 
-FROM loc1 
-INNER JOIN folio_circulation.check_in__t 
-	ON loc1.item_id = check_in__t.item_id 
-	AND loc1.most_recent_check_in = check_in__t.occurred_date_time
-	
-INNER JOIN folio_inventory.service_point__t 
-	ON check_in__t.service_point_id = service_point__t.id
-	
-WHERE CURRENT_DATE::DATE - loc1.most_recent_check_in::DATE > 10
+FROM recs
+WHERE (recs.days_since_requested > 10 OR recs.days_since_requested IS NULL)
 
-ORDER BY library_name, effective_location_name, effective_shelving_order COLLATE "C", call_number
+ORDER BY recs.library_name, recs.holdings_location_name, size_group, recs.shelving_order collate "C"
 ;
