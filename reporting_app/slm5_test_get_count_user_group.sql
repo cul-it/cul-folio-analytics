88f14e4c5de435483ed9258812266422b087dea9
Sharon Markus <36549480+sbeltaine@users.noreply.github.com>
2025-04-30 16:09:00 -0400

    Update slm5_test_get_count_user_group.sql

--- reporting_app/slm5_test_get_count_user_group.sql
+++ reporting_app/slm5_test_get_count_user_group.sql
@@ -1,9 +1,8 @@
 --ldp:function get_count_user_group
 
+DROP FUNCTION IF EXISTS z_reporting.test_get_count_user_group;
 
-DROP FUNCTION IF EXISTS local_core.slm5_test_get_count_user_group;
-
-CREATE FUNCTION local_core.slm5_test_get_count_user_group(
+CREATE FUNCTION z_reporting.test_get_count_user_group(
     param_user_group TEXT DEFAULT ''
 )
 RETURNS TABLE (
