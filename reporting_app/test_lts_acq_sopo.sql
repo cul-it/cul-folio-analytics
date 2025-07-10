8758a977a0b7e12ae2420c04d7c0f4e8ae330355
Sharon Markus <36549480+sbeltaine@users.noreply.github.com>
2025-07-10 15:25:19 -0400

    Update test_lts_acq_sopo.sql

--- reporting_app/test_lts_acq_sopo.sql
+++ reporting_app/test_lts_acq_sopo.sql
@@ -1,12 +1,42 @@
-SELECT
-    so_po,
-    COUNT(*) AS po_count
-FROM folio_acq.orders
-WHERE created_date >= :start_date
-  AND created_date < :end_date
-  AND (
-    :so_po_text IS NULL OR
-    so_po ILIKE '%' || :so_po_text || '%'
-  )
-GROUP BY so_po
-ORDER BY so_po;
+DROP FUNCTION IF EXISTS TEST_LTS_ACQ_sopo;
+CREATE FUNCTION TEST_LTS_ACQ_sopo(
+    start_date date DEFAULT '2021-07-01',
+    end_date date DEFAULT '2050-01-01')
+RETURNS TABLE (
+  pol_instance_hrid text, 
+  po_number text, 
+  po_line_number text, 
+  vendor_code text, 
+  f_status text,
+  po_created_date date, 
+  field text, series text)
+AS $$
+  SELECT 
+    poi.pol_instance_hrid,
+    poi.po_number,
+    poi.po_line_number,
+    poi.vendor_code,
+    poi.po_workflow_status,
+    poi.created_date,
+    marc.field,
+    string_agg(DISTINCT marc.content, '') AS series
+  FROM folio_derived.po_instance AS poi
+  LEFT JOIN folio_inventory.instance__t__ AS ii 
+    ON poi.pol_instance_hrid = ii.hrid 
+  LEFT JOIN folio_source_record.marc__t AS marc 
+    ON ii.hrid = marc.instance_hrid 
+  WHERE marc.field IN ('490','830') 
+    AND poi.created_date::DATE BETWEEN start_date AND end_date
+  GROUP BY 
+    poi.pol_instance_hrid,
+    marc.instance_hrid,
+    poi.po_number,
+    poi.po_line_number,
+    poi.vendor_code,
+    poi.created_date,
+    poi.po_workflow_status,
+    marc.field;
+$$
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE;
