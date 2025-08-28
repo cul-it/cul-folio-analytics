33fb66a247e4c281d90e8d5d16b713ca8033a234
Sharon Markus <36549480+sbeltaine@users.noreply.github.com>
2025-08-28 14:27:13 -0400

    Update ytd_acct_bal_by_ledger_univ_acct.sql

--- metadb/canned_reports/MCR132/ytd_acct_bal_by_ledger_univ_acct.sql
+++ metadb/canned_reports/MCR132/ytd_acct_bal_by_ledger_univ_acct.sql
@@ -1,15 +1,16 @@
 --MCR132 
 --YTD_acct_bal_by_ledger_univ_acct
---last updated: 8/12/24
+--last updated: 8/28/24
 --written by Nancy Bolduc, revised to Metadb by Ann Crowley and Sharon Markus
 --This report provides the year-to-date external account cash balance along with total_expenditures, initial allocation, and net allocation. 
 --The fiscal year can be selected in the WHERE clause. 
+--8-28-25: added or subtracted credits into the YTD_expenditures and cash_balance calculations
 
 SELECT
     CURRENT_DATE,
     fl.name AS finance_ledger_name,
     ff.external_account_no AS external_account,
-    SUM(COALESCE (fb.expenditures,0)) AS YTD_expenditures,
+    SUM(COALESCE (fb.expenditures,0) - coalesce (fb.credits,0)) AS YTD_expenditures, -- subtracted credits
     SUM(COALESCE (fb.initial_allocation,0)) AS initial_allocation,
     SUM(COALESCE (fb.initial_allocation,0) + COALESCE (fb.allocation_to,0)
         -COALESCE (fb.allocation_from,0)) AS total_allocated,
@@ -17,7 +18,8 @@ SELECT
         -COALESCE (fb.allocation_from,0)+COALESCE (fb.net_transfers,0)) AS total_funding,
     SUM(COALESCE (fb.initial_allocation,0)+COALESCE (fb.allocation_to,0)
         -COALESCE (fb.allocation_from,0)+COALESCE (fb.net_transfers,0)
-        -COALESCE (fb.expenditures,0)) AS cash_balance -- This balance excludes encumbrances and awaiting payment
+        -COALESCE (fb.expenditures,0)) 
+        +coalesce (fb.credits,0)) AS cash_balance -- This balance excludes encumbrances and awaiting payment -- added credits
 FROM
     folio_finance.fund__t AS ff
     LEFT JOIN folio_finance.budget__t AS fb ON fb.fund_id = ff.id  
@@ -25,7 +27,7 @@ FROM
     LEFT JOIN folio_finance.ledger__t AS fl ON fl.id::UUID = ff.ledger_id
 WHERE
     ff.fund_status LIKE 'Active'
-    AND ffy.code LIKE 'FY2025'
+    AND ffy.code LIKE 'FY2026'
 GROUP BY
     external_account_no,
     fl.name
