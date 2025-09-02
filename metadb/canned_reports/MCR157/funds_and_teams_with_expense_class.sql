d7479f8887d1bde7195b6664d9f3599dfcbdffe1
Sharon Markus <36549480+sbeltaine@users.noreply.github.com>
2025-09-02 13:26:17 -0400

    Update funds_and_teams_with_expense_class.sql

--- metadb/canned_reports/MCR157/funds_and_teams_with_expense_class.sql
+++ metadb/canned_reports/MCR157/funds_and_teams_with_expense_class.sql
@@ -6,10 +6,14 @@
 -- Posted on: 7/25/24
 -- 10-2-24: corrected the query to add a join from expense class fiscal year code to finance fiscal year code
 -- 1-24-25: replaced derived table finance_transaction_invoices with derivation for derived table; gets up-to-the-minute data
+-- 8-15-25: revised to account for budget__t.credits in calculations
+-- 8-18-25: took out "Pending payment" as transaction type criteria in line 64 (doesn't exist anymore); added "credited" column (line 110)
+-- 8-28-25: subtracted credits from expenditures, including for percent spent in expense class
+-- 9-2-25: renamed "expenditures" to "net_expenditures"
 
 WITH parameters AS (
     SELECT
-        '' AS fiscal_year_code,
+        'FY2026' AS fiscal_year_code,
         '' as team_filter
 ),
 
@@ -19,11 +23,9 @@ SELECT
     ft.id AS transaction_id,
     jsonb_extract_path_text(ft.jsonb, 'amount')::numeric(19,4) AS transaction_amount,
     CASE WHEN jsonb_extract_path_text(ft.jsonb, 'transactionType') = 'Credit'
-        THEN 
-            jsonb_extract_path_text(ft.jsonb, 'amount') :: NUMERIC(19,4) * -1
-        ELSE 
-            jsonb_extract_path_text(ft.jsonb, 'amount') :: NUMERIC(19,4)
-    END AS effective_transaction_amount,
+        THEN jsonb_extract_path_text(ft.jsonb, 'amount') :: NUMERIC(19,4) * -1
+        ELSE jsonb_extract_path_text(ft.jsonb, 'amount') :: NUMERIC(19,4)
+    	END AS effective_transaction_amount,
     jsonb_extract_path_text(ft.jsonb, 'currency') AS transaction_currency,
     jsonb_extract_path_text(ft.jsonb, 'metadata', 'createdDate')::date AS transaction_created_date,
     jsonb_extract_path_text(ft.jsonb, 'metadata', 'updatedDate')::date AS transaction_updated_date,
@@ -60,24 +62,23 @@ FROM
     LEFT JOIN folio_finance.fund AS tf ON ft.tofundid = tf.id
     LEFT JOIN folio_finance.budget AS fb ON ft.fromfundid = fb.fundid AND ft.fiscalyearid = fb.fiscalyearid
     LEFT JOIN folio_organizations. organizations AS oo ON jsonb_extract_path_text(ii.jsonb, 'vendorId')::uuid = oo.id
-WHERE (jsonb_extract_path_text(ft.jsonb, 'transactionType') = 'Pending payment'
-    OR jsonb_extract_path_text(ft.jsonb, 'transactionType') = 'Payment'
-    OR jsonb_extract_path_text(ft.jsonb, 'transactionType') = 'Credit')
+WHERE jsonb_extract_path_text(ft.jsonb, 'transactionType') = 'Payment'
+    OR jsonb_extract_path_text(ft.jsonb, 'transactionType') = 'Credit' -- removed transaction type "Pending payment" from criteria, since it doesn't exist anymore
 ),
         
 expense_class AS    
 (
-SELECT distinct      
+SELECT DISTINCT      
 	fti.effective_fund_code,
         fti.effective_fund_name,
         fec.name AS expense_class_name,
         ffy.code AS expense_class_fiscal_year_code,
-        sum(case when ft.transaction_type = 'Credit' THEN fti.transaction_amount*-1 ELSE fti.transaction_amount END) as amount_spent_in_expense_class
+        SUM (CASE WHEN ft.transaction_type = 'Credit' THEN fti.transaction_amount*-1 ELSE fti.transaction_amount END) as amount_spent_in_expense_class
         
-FROM fintraninv as fti --folio_derived.finance_transaction_invoices as fti  
-        left join folio_finance.transaction__t as ft on fti.transaction_id = ft.id --LEFT JOIN finance_transactions AS ft ON fti.transaction_id = ft.id 
-        left join folio_finance.expense_class__t as fec on fti.transaction_expense_class_id = fec.id --LEFT JOIN finance_expense_classes AS fec ON fti.transaction_expense_class_id = fec.id
-        left join folio_finance.fiscal_year__t as ffy on ffy.id = fti.transaction_fiscal_year_id --LEFT JOIN finance_fiscal_years AS ffy ON ffy.id = fti.transaction_fiscal_year_id   
+FROM fintraninv as fti  
+        LEFT JOIN folio_finance.transaction__t as ft on fti.transaction_id = ft.id 
+        LEFT JOIN folio_finance.expense_class__t as fec on fti.transaction_expense_class_id = fec.id 
+        LEFT JOIN folio_finance.fiscal_year__t as ffy on ffy.id = fti.transaction_fiscal_year_id   
 
 GROUP BY 
         fti.effective_fund_code,
@@ -87,43 +88,73 @@ GROUP BY
 )
 
 SELECT 
-        current_date,
-        fgffy.fiscal_year_id,
-        ffy.code AS fiscal_year,
-        fg.name AS team,
-        fg.description AS fund_manager,
-        fft.name AS fund_type,
-        ff.code AS finance_funds_code,
-        ff.name AS finance_funds_name,
-        ff.description,
-        COALESCE (fb.net_transfers,0) AS net_transfers,
-	(COALESCE (fb.initial_allocation,0)+COALESCE (fb.allocation_to,0)-COALESCE (fb.allocation_from,0)+COALESCE (fb.net_transfers,0)) AS total_funding,
-	COALESCE (fb.expenditures,0) AS expenditures,
+    current_date,
+    fgffy.fiscal_year_id,
+    ffy.code AS fiscal_year,
+    fg.name AS team,
+    fg.description AS fund_manager,
+    fft.name AS fund_type,
+    ff.code AS finance_funds_code,
+    ff.name AS finance_funds_name,
+    ff.description,
+    COALESCE (fb.net_transfers,0) AS net_transfers,
+        
+	(COALESCE (fb.initial_allocation,0)
+		+ COALESCE (fb.allocation_to,0)
+		- COALESCE (fb.allocation_from,0)
+		+ COALESCE (fb.net_transfers,0)
+		) AS total_funding,
+		
+	COALESCE (fb.expenditures,0) - coalesce (fb.credits,0) AS net_expenditures, -- subtracted credits
 	COALESCE (fb.encumbered,0) AS encumbered,
 	COALESCE (fb.awaiting_payment,0) AS awaiting_payment,
-	(COALESCE (fb.encumbered,0)+COALESCE (fb.awaiting_payment,0)+COALESCE (fb.expenditures,0)) AS unavailable,-- Total of amount Encumbered, Awaiting Payment and Expended
-	(COALESCE (fb.initial_allocation,0)+COALESCE (fb.allocation_to,0)-COALESCE (fb.allocation_from,0)+COALESCE (fb.net_transfers,0)) - COALESCE (fb.expenditures,0) AS cash_balance,
-	(COALESCE (fb.initial_allocation,0)+COALESCE (fb.allocation_to,0)-COALESCE (fb.allocation_from,0)+COALESCE (fb.net_transfers,0)) - COALESCE (fb.encumbered,0) - COALESCE (fb.awaiting_payment,0) - COALESCE (fb.expenditures,0) AS available_balance,
-        ec.expense_class_name,
-        ec.amount_spent_in_expense_class, --This amount includes amount Expended and Awaiting Payment
-        CASE WHEN 
-        	fb.expenditures > 0                 
-                THEN ec.amount_spent_in_expense_class/fb.expenditures
-                ELSE 0 
-                END AS percent_of_fund_spent_on_expense_class_to_date
+	
+	COALESCE (fb.credits,0) as credited,
+	
+	(COALESCE (fb.encumbered,0)
+		+ COALESCE (fb.awaiting_payment,0)
+		+ COALESCE (fb.expenditures,0)
+		- coalesce (fb.credits,0)
+		) AS unavailable,-- Total of amount Encumbered, Awaiting Payment and Expended -- subtracted credits
+
+	(COALESCE (fb.initial_allocation,0)
+		+ COALESCE (fb.allocation_to,0)
+		- COALESCE (fb.allocation_from,0)
+		+ COALESCE (fb.net_transfers,0)
+		) 
+		- COALESCE (fb.expenditures,0) 
+		+ coalesce (fb.credits,0) AS cash_balance, -- added credits
+		
+	(COALESCE (fb.initial_allocation,0)
+		+ COALESCE (fb.allocation_to,0)
+		- COALESCE (fb.allocation_from,0)
+		+ COALESCE (fb.net_transfers,0)
+		) 
+		- COALESCE (fb.encumbered,0) 
+		- COALESCE (fb.awaiting_payment,0) 
+		- COALESCE (fb.expenditures,0) 
+		+ coalesce (fb.credits,0) AS available_balance, -- added credits
+		
+    ec.expense_class_name,
+    ec.amount_spent_in_expense_class, --This amount includes amount Expended and Awaiting Payment
+    CASE WHEN 
+        (fb.expenditures - fb.credits) > 0  -- subtracted credits from expenditures               
+            THEN ec.amount_spent_in_expense_class/(fb.expenditures - fb.credits) -- subtracted credits from expenditures
+            ELSE 0 
+            END AS percent_of_fund_spent_on_expense_class_to_date 
 FROM 
-	folio_finance.fund__t as ff--finance_funds AS ff     
-       left join folio_finance.budget__t as fb on fb.fund_id = ff.id --LEFT JOIN finance_budgets AS fb ON fb.fund_id = ff.id  
-       left join folio_finance.fiscal_year__t as ffy on fb.fiscal_year_id = ffy.id --LEFT JOIN finance_fiscal_years AS ffy ON fb.fiscal_year_id = ffy.id
-       left join folio_finance.group_fund_fiscal_year__t as fgffy on fgffy.budget_id = fb.id --LEFT JOIN finance_group_fund_fiscal_years AS fgffy  ON fgffy.budget_id = fb.id
-       left join folio_finance.groups__t as fg on fg.id = fgffy.group_id --LEFT JOIN finance_groups AS fg ON fg.id = fgffy.group_id
-       left join folio_finance.fund_type__t as fft on ff.fund_type_id = fft.id --LEFT JOIN finance_fund_types AS fft ON ff.fund_type_id = fft.id
-       left join expense_class as ec on ff.code = ec.effective_fund_code --LEFT JOIN expense_class AS ec ON ff.code = ec.effective_fund_code
-       	and ec.expense_class_fiscal_year_code = ffy.code -- added 10-2-24    
+	folio_finance.fund__t AS ff    
+       LEFT JOIN folio_finance.budget__t AS fb ON fb.fund_id = ff.id   
+       LEFT JOIN folio_finance.fiscal_year__t AS ffy ON fb.fiscal_year_id = ffy.id 
+       LEFT JOIN folio_finance.group_fund_fiscal_year__t AS fgffy ON fgffy.budget_id = fb.id 
+       LEFT JOIN folio_finance.groups__t AS fg ON fg.id = fgffy.group_id 
+       LEFT JOIN folio_finance.fund_type__t AS fft ON ff.fund_type_id = fft.id 
+       LEFT JOIN expense_class AS ec ON ff.code = ec.effective_fund_code
+       	 AND ec.expense_class_fiscal_year_code = ffy.code -- added 10-2-24    
 
 WHERE 
-	((ffy.code = (SELECT fiscal_year_code FROM parameters) or (select fiscal_year_code from parameters) = ''))
-	and ((fg.name = (select team_filter from parameters) or (select team_filter from parameters) =''))
+	((ffy.code = (SELECT fiscal_year_code FROM parameters) OR (SELECT fiscal_year_code FROM parameters) = ''))
+	AND ((fg.name = (SELECT team_filter FROM parameters) OR (SELECT team_filter FROM parameters) =''))
 	
 ORDER BY 
 	team, finance_funds_code, finance_funds_name, expense_class_name
