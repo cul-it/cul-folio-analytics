966e7a11793d8beb6d7591c8216fa468b022236a
Natalya Pikulik <62571646+natalyapik@users.noreply.github.com>
2026-05-28 13:49:21 -0400

    Create instance_series.sql

---
+++ metadb/derived_table_updates/instance_series.sql
@@ -0,0 +1,17 @@
+--metadb:table instance_series
+
+-- This derived table extracts series statements from the FOLIO
+-- Instance record
+
+DROP TABLE IF EXISTS instance_series;
+
+CREATE TABLE instance_series AS 
+
+SELECT 
+    i.id AS instance_id,
+    jsonb_extract_path_text(i.jsonb, 'hrid') AS instance_hrid,
+    s.jsonb ->> 'value' AS series,
+    s.ordinality AS series_ordinality
+FROM 
+    folio_inventory.instance AS i
+    CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(i.jsonb, 'series')) WITH ORDINALITY AS s (jsonb);
