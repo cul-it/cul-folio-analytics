074147e097ad1372bbb33a353ee449e01b48b761
Vandana Shah <vp25@cornell.edu>
2026-06-25 19:37:21 -0400

    Update flattened_formats.sql

--- metadb/canned_reports/MCR420/flattened_formats.sql
+++ metadb/canned_reports/MCR420/flattened_formats.sql
@@ -7,26 +7,25 @@
 --Table number 3
 
 -- Create flattened table (not view) with all fields from vs_primary_formats
-DROP TABLE IF EXISTS local_statistics.vs_primary_formats_flattened;
+--DROP TABLE IF EXISTS local_statistics.vs_primary_formats_flattened;
 CREATE TABLE local_statistics.vs_primary_formats_flattened AS
 SELECT 
     -- All original fields from vs_primary_formats
-    f.instance_id,
+	CURRENT_DATE AS table_create_date,    
+	f.instance_id,
     f.record_type_06,
     f.bib_level_07,
     f.field_007_00,
     f.field_008_21,
     f.marc_245h,                    -- Keep as array
     f.marc_948f, 					-- Keep as array
-     marc_653a,						-- Keep as array
     f.instance_stat_codes,          -- Keep as array
     f.special_format_codes,         -- Keep as array
     f.title,
     f.primary_format,
     f.is_microform,
     f.is_electronic,
-    f.is_thesis,
-    
+        
     -- Flattened holdings fields (from arrays to individual values)
     loc_code as location_code,
     lib_name as library_name,
@@ -49,6 +48,9 @@ CREATE INDEX idx_vs_primary_formats_flattened_location ON local_statistics.vs_pr
 CREATE INDEX idx_vs_primary_formats_flattened_electronic ON local_statistics.vs_primary_formats_flattened(is_electronic);
 CREATE INDEX idx_vs_primary_formats_flattened_microform ON local_statistics.vs_primary_formats_flattened(is_microform);
 CREATE INDEX idx_vs_primary_formats_flattened_call_status ON local_statistics.vs_primary_formats_flattened(call_number_status);
+
+--The "combo" in the index name below refers to a combination index (also called a composite index). 
+--This index is built on multiple columns together: instance_id AND primary_format.
 CREATE INDEX idx_vs_primary_formats_flattened_combo ON local_statistics.vs_primary_formats_flattened(instance_id, primary_format);
 CREATE INDEX idx_vs_primary_formats_flattened_lib_format ON local_statistics.vs_primary_formats_flattened(library_name, primary_format);
 
