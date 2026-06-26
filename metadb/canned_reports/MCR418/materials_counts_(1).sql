d04f1a191009bf7fad50595b6c8847656ac04f2c
Vandana Shah <vp25@cornell.edu>
2026-06-26 14:03:22 -0400

    Update materials_counts_(1).sql

--- metadb/canned_reports/MCR418/materials_counts_(1).sql
+++ metadb/canned_reports/MCR418/materials_counts_(1).sql
@@ -14,27 +14,50 @@ DROP TABLE IF EXISTS local_statistics.vs_primary_formats_flattened;
 --Table 1
 CREATE TABLE local_statistics.vs_marc_data_prep AS
 WITH bibliographic_data AS (
-    -- Extract bibliographic-level MARC data (one record per instance)
+    -- First get all instances that have any of the target fields
+    SELECT DISTINCT instance_id FROM local_derived.marc__t 
+    WHERE field IN ('000', '007', '008', '245', '948')
+),
+
+marc_control_fields AS (
     SELECT DISTINCT 
         instance_id,
-        -- Leader positions 6-7 (field = '000') - single values per instance
         MAX(CASE WHEN field = '000' THEN substring(content, 7, 1) END) as record_type_06,
         MAX(CASE WHEN field = '000' THEN substring(content, 8, 1) END) as bib_level_07,
-        
-        -- Control fields - single values per instance  
-        MAX(CASE WHEN field = '007' AND length(content) > 0 
-            THEN substring(content, 1, 1) END) as field_007_00,
+        -- Changed to array for repeatable 007 field
+        array_agg(DISTINCT substring(content, 1, 1)) FILTER (
+            WHERE field = '007' AND length(content) > 0
+        ) as field_007_00,
         MAX(CASE WHEN field = '008' AND length(content) > 21 
-            THEN substring(content, 22, 1) END) as field_008_21,
-            
-        -- Bibliographic fields that can have multiple occurrences
-        array_agg(DISTINCT content) FILTER (WHERE field = '245' AND sf = 'h') as marc_245h,
-        array_agg(DISTINCT content) FILTER (WHERE field = '948' AND sf = 'f') as marc_948f,
-                     
+            THEN substring(content, 22, 1) END) as field_008_21
     FROM local_derived.marc__t 
-    WHERE field IN ('000', '007', '008', '245', '502', '948')  
-      AND (sf IN ('h', 'f') OR sf IS NULL)  
+    WHERE field IN ('000', '007', '008')
     GROUP BY instance_id
+),
+
+
+marc_data_fields AS (
+    SELECT DISTINCT 
+        instance_id,
+        array_agg(DISTINCT content) FILTER (WHERE field = '245' AND sf = 'h') as marc_245h,
+        array_agg(DISTINCT content) FILTER (WHERE field = '948' AND sf = 'f') as marc_948f
+    FROM local_derived.marc__t 
+    WHERE (field = '245' AND sf = 'h') OR (field = '948' AND sf = 'f')
+    GROUP BY instance_id
+),
+
+final_bibliographic_data AS (
+    SELECT 
+        b.instance_id,
+        c.record_type_06,
+        c.bib_level_07,
+        c.field_007_00,
+        c.field_008_21,
+        d.marc_245h,
+        d.marc_948f
+    FROM bibliographic_data b
+    LEFT JOIN marc_control_fields c ON b.instance_id = c.instance_id
+    LEFT JOIN marc_data_fields d ON b.instance_id = d.instance_id
 ),
 
 holdings_data AS (
@@ -86,7 +109,7 @@ statistical_codes AS (
 
 -- Final combination
 SELECT 
-	CURRENT_DATE AS table_create_date,
+    CURRENT_DATE AS table_create_date,
     b.instance_id,
     b.record_type_06,
     b.bib_level_07,
@@ -101,7 +124,7 @@ SELECT
     i.title,
     COALESCE(sc.instance_stat_codes, '{}') as instance_stat_codes,
     COALESCE(sc.special_format_codes, '{}') as special_format_codes
-FROM bibliographic_data b
+FROM final_bibliographic_data b
 INNER JOIN instance_metadata i ON b.instance_id = i.instance_id
 LEFT JOIN holdings_data h ON b.instance_id = h.instance_id
 LEFT JOIN statistical_codes sc ON b.instance_id = sc.instance_id;
@@ -109,4 +132,3 @@ LEFT JOIN statistical_codes sc ON b.instance_id = sc.instance_id;
 -- Create indexes
 CREATE INDEX idx_vs_marc_data_prep_instance ON local_statistics.vs_marc_data_prep(instance_id);
 CREATE INDEX idx_vs_marc_data_prep_gin_locations ON local_statistics.vs_marc_data_prep USING gin(location_code);
-
