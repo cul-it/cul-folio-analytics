0d4a965735d1b25e2ea859177d734e7a0227c064
Sharon Markus <36549480+sbeltaine@users.noreply.github.com>
2026-07-10 10:51:52 -0400

    Update marct_srs_id_fix.sql

--- metadb/system_queries/MSQ105/marct_srs_id_fix.sql
+++ metadb/system_queries/MSQ105/marct_srs_id_fix.sql
@@ -1,37 +1,60 @@
---MSQ105
---last updated: 3/4/26
+--marct_srs_id_fix
+--daily automated MSQ105 to create new fixed marc__t table in local_derived schema
+--last updated: 4/17/26
 --written by: Joanne Leary
 --This query fixes the marc__t table so it has only the most recent srs_id
 --2/9/26: adapted by Sharon Markus to publish marc__t table in local_derived
 --3/4/26: added WHERE clauses to exclude NULL records
+--4/17/26: more changes to keep records from dropping out
 
-DROP TABLE IF EXISTS local_derived.marc__t;
-
+DROP TABLE IF EXISTS local_derived.marc__t CASCADE;
 CREATE TABLE local_derived.marc__t AS
 
--- 1. Find the max record in the records_lb table for every external_id
+-- 1. Get the max row number for each instance record in the records_lb table
+ 
 WITH recs AS
-     (SELECT
-       records_lb.external_id,
-       MAX (records_lb.__id) AS max_record
+        (SELECT
+                records_lb.external_hrid,
+                MAX (records_lb.__id) AS max_record
 
-FROM folio_source_record.records_lb
-   WHERE records_lb.external_id IS NOT NULL
+        FROM folio_source_record.records_lb
+        --WHERE records_lb.external_hrid in ('15058802','10016344')
+           WHERE records_lb.state = 'ACTUAL'
 
-GROUP BY records_lb.external_id
+        GROUP BY records_lb.external_hrid
+        ),
 
-)
+-- 2. Get the record_id associated with the max row number in the records_lb table
 
--- 2. Join the max record from the first query to the records_lb table, 
--- which is inner joined by srs_id to the marc__t table
+recs2 AS
+        (SELECT
+                records_lb.external_hrid,
+                records_lb.id,
+                recs.max_record
+
+        FROM folio_source_record.records_lb
+                INNER JOIN recs
+                ON recs.max_record = records_lb.__id
+ 
+        WHERE records_lb.state = 'ACTUAL'
+        )
+
+-- 3. Join the marc__t table's srs_id to the records_lb.id from the recs2 query
 
 SELECT
-    mt.*    -- get all marc__t fields
+   mt.*,
+   recs2.id AS max_row_number_from_records_lb
 
 FROM folio_source_record.marc__t as mt
-INNER JOIN folio_source_record.records_lb
-   ON mt.srs_id = records_lb.id
-INNER JOIN recs
-   ON records_lb.__id = recs.max_record    -- join the records_lb table to the max_record from the recs result
-   WHERE records_lb.external_id IS NOT NULL
+        INNER JOIN recs2
+        ON mt.instance_hrid = recs2.external_hrid
+          AND mt.srs_id = recs2.id
+--WHERE mt.instance_hrid in ('15058802','10016344')
+
+ORDER BY 
+mt.instance_hrid, 
+mt.field, 
+mt.sf
+
 ;
+
