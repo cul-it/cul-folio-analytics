ef15573cbaad9227ef303d533bd2e824a0e4cbff
Sharon Markus <36549480+sbeltaine@users.noreply.github.com>
2026-08-18 14:51:25 -0400

    Update instance_series.sql

--- local_derived/instance_series.sql
+++ local_derived/instance_series.sql
@@ -15,5 +15,5 @@ SELECT
 FROM 
     folio_inventory.instance AS i
     CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(i.jsonb, 'series')) 
-    WITH ORDINALITY AS s (jsonb, ordinality)
+    WITH ORDINALITY AS s(jsonb, ordinality)
     ;
